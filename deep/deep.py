34bca248d4e82f1fc6c76f7ca1e4bba3a1d6e90a
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-09-04 12:17:56 +0000

    Mon, Sep 4, 2023, 3:17 PM +03:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,6 +1,6 @@
 a = input("What is the Answer to the Great Question of Life, the Universe, and Everything?")
-a = a.strip()
-a = a.lowercase()
+a = a.strip().lower().remove()
+
 match a :
     case "42"|"Forty Two"|"forty-two"|"forty two"|"Forty two":
         print("Yes")
