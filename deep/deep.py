48f8fbf19650fdfd7a0000ef638e6c3e6b1958a3
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-09-04 12:11:16 +0000

    Mon, Sep 4, 2023, 3:11 PM +03:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,4 +1,4 @@
-a = input()
+a = input("What is the Answer to the Great Question of Life, the Universe, and Everything?")
 
 match a :
     case "42"|"Forty Two"|"forty-two"|"forty two"|"Forty two":
