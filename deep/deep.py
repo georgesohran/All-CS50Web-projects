f55eb2f989c2ec0ae29ff65aff28bd8e16995089
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-09-05 15:24:16 +0000

    Tue, Sep 5, 2023, 6:24 PM +03:00

--- deep/deep.py
+++
@@ -1,8 +0,0 @@
-a = input("What is the Answer to the Great Question of Life, the Universe, and Everything?")
-a = a.lower().replace("-","").replace(" ","")
-
-match a :
-    case "42"|"fortytwo":
-        print("Yes")
-    case _:
-        print("No")
