25a29be7d5f3fbc01e23fc7a47fb93818037735b
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-09-05 16:02:57 +0000

    Tue, Sep 5, 2023, 7:02 PM +03:00

--- bank/bank.py
+++ bank/bank.py
@@ -1,10 +1,10 @@
-g = input()
+g = input("Greeting: ")
 g = g.lower()
 
 
 if g.find("hello",0,5) != -1:
     print("$0")
-elif g.find("h",0,2) != -1:
+elif g.find("h",0,1) != -1:
     print("$20")
 else:
     print("$100")
