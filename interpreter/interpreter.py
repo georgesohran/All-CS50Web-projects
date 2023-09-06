d663bb6e5929ce63dec347f92aa556e19265bb0c
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-09-06 17:32:46 +0000

    Wed, Sep 6, 2023, 8:32 PM +03:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -1,3 +1,15 @@
 expression = input().replace(" ","")
 
-x,y,z = expression.split("")
+x,y,z = expression.split()
+x = int(x)
+z = int(z)
+
+match y:
+    case "/":
+        print(float(x/z))
+    case "+":
+        print(float(x+z))
+    case "-":
+        print(float(x-z))
+    case "*":
+        print(float(x*z))
