01ead9065db091642cc483ba9e2bc10b4e525d94
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-09-06 16:57:48 +0000

    Wed, Sep 6, 2023, 7:57 PM +03:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,4 +1,18 @@
 file = input()
+i = "image/"
 
 if file.endswith("gif"):
-    print()
+    print(i+"gif")
+if file.endswith("jpg"):
+    print(i+"jpg")
+if file.endswith("jpeg"):
+    print(i+"jpeg")
+if file.endswith("png"):
+    print(i+"png")
+if file.endswith("pdf"):
+    print(i+"pdf")
+if file.endswith("txt"):
+    print(i+"txt")
+if file.endswith("zip"):
+    print(i+"zip")
+
