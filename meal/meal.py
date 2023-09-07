23aaccc240f22d78413d3b5313e74310693218a0
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-09-07 04:30:17 +0000

    Thu, Sep 7, 2023, 7:30 AM +03:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,12 +1,14 @@
 def main():
     t = input("What time is it?")
+    
 
 
 def convert(time):
     hour,min = time.split(":")
     hour = float(hour)
     min = float(min)
-    min10 = (min*100)/60
+    min10 = ((min*100)/60)*0.01
+    return hour + min10
 
 
 if __name__ == "__main__":
