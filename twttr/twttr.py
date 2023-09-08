f85aecc0c794b6ab880b49c368123963aaa1f5f5
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-09-08 12:36:38 +0000

    Fri, Sep 8, 2023, 3:36 PM +03:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,1 +1,9 @@
+def convert(w):
+    for c in w:
+        if c == "a"or"e"or"u"or"i"or"o":
+            w = w.replace(c,"")
+    return w
+
 word = input("Input: ").lower()
+word = convert(word)
+print("Output:",word)
