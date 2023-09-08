9a9156398d4912dde1480ece10c2a77837722945
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-09-08 09:24:24 +0000

    Fri, Sep 8, 2023, 12:24 PM +03:00

--- camel/camel.py
+++
@@ -1,9 +0,0 @@
-def camel_to_snake(cam):
-    for cer in cam:
-        if cer.isupper():
-            cam = cam.replace(cer,"_"+cer.lower())
-    return cam
-
-camel = input("camelCase: ")
-snake = camel_to_snake(camel)
-print("snake_case:",snake)
