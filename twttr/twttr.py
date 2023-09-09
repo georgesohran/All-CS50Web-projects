fd80eedf47117f8e78b7e11ea53031b5dbaae7dd
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-09-09 11:27:33 +0000

    Sat, Sep 9, 2023, 2:27 PM +03:00

--- twttr/twttr.py
+++
@@ -1,9 +0,0 @@
-def convert(w):
-    for c in w:
-        if c in "aoieuAOIEU":
-            w = w.replace(c,"")
-    return w
-
-word = input("Input: ")
-word = convert(word)
-print("Output:",word)
