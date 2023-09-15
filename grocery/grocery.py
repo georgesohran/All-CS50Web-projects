b9e68e30ca2d18867ba072089d892d63f25864e9
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-09-15 12:30:57 +0000

    Fri, Sep 15, 2023, 3:30 PM +03:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -1,9 +1,10 @@
-all_items = {}
+added_items = {}
 
 
 while True :
     try:
         item = input()
-        
+        if item not in added_items:
+            added_items.
     except EOFError:
         break
