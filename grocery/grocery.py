162679f0ee6c7c96f08c0e18ebe3f11b28b9094e
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-09-16 16:19:57 +0000

    Sat, Sep 16, 2023, 7:19 PM +03:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -12,6 +12,9 @@ while True :
         else:
             added_items[item]+=1
     except EOFError:
+        sorted_keys = sorted(added_items.items())
+        for k in sorted_keys:
+            added_items
         for item in added_items:
             print(added_items[item],item.upper())
         break
