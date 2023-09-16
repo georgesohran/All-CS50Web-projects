01bac021c762cc181603885938baad0759f29be3
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-09-16 16:31:52 +0000

    Sat, Sep 16, 2023, 7:31 PM +03:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -1,16 +1,15 @@
 added_items = {}
 
-def add_item(it):
-    added_items[it] = 0
-
+def add_item(item):
+    added_items[item] = 0
+    if item not in added_items:
+        added_items[item] = 1
+    else:
+        added_items[item] += 1
 while True :
     try:
         item = input()
-        if item not in added_items:
-            add_item(item)
-            added_items[item]+=1
-        else:
-            added_items[item]+=1
+        add_item()
     except EOFError:
         for item in added_items:
             print(added_items[item],item.upper())
