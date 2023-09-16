d4f1349d70fee58eb4e0619ff11b76e593dfafbb
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-09-16 16:46:34 +0000

    Sat, Sep 16, 2023, 7:46 PM +03:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -2,7 +2,8 @@ added_items = {}
 
 def add_item(item):
     if item not in added_items:
-        added_items[item] = 1
+        added_items[item] = 0
+        added_items[item] += 1
     else:
         added_items[item] += 1
 
