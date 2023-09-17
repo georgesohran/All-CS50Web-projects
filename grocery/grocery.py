2e8c87a06eb275d3ff09431b5dd0ed97d680c0d9
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-09-17 11:05:46 +0000

    Sun, Sep 17, 2023, 2:05 PM +03:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -6,19 +6,19 @@ def add_item(item):
     else:
         added_items[item] += 1
 
+def sort_and_print_items():
+    sorted_items_names = sorted(added_items.keys())
+    for k in sorted_items_names:
+        added_items[k] = added_items.pop(k)
+    for item in added_items:
+        print(added_items[item],item.upper())
 
 while True :
     try:
         item = input()
         add_item(item)
     except EOFError:
-        c = 0
-        sorted_items_names = sorted(added_items.keys())
-        for k in sorted_items_names:
-#            pv = added_items.pop(k)
-            added_items[k] = added_items.pop(k)
-        for item in added_items:
-            print(added_items[item],item.upper())
+        sort_and_print_items()
         break
     except KeyError:
         break
