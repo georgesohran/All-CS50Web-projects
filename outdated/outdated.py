19b3821cd083dfb58da1eb08749f8c3bbd026834
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-09-18 12:57:46 +0000

    Mon, Sep 18, 2023, 3:57 PM +03:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -13,28 +13,15 @@ month =[
     "December"
 ]
 
-def is_correct_format(s):
-    try:
-        try:
-            month1,day,year = s.split("/")
-        except ValueError:
-            for m in month:
-                if s.startswith(m):
-                    month1 = month.index(m)+1
-                    s.replace(m,'')
-                    day,year = s.split(",")
-        return True
-    except ValueError:
-        return False
 
 
 while True:
     ordate = input("Date: ").replace(" ","")
-    if is_correct_format(ordate):
+    try:
+        month1,day,year = ordate.split("/")
+        month1,day,year = int(month1),int(day),int(year)
+    except ValueError:
         try:
-            month1,day,year = ordate.split("/")
-            month1,day,year = int(month1),int(day),int(year)
-        except ValueError:
             for m in month:
                 if ordate.startswith(m):
                     month1 = month.index(m)+1
@@ -42,15 +29,16 @@ while True:
                     day,year = ordate.split(",")
                     day,year = int(day),int(year)
                     break
-        except EOFError:
-            break
-
-        if (0<month1<=12) and (0<day<=31):
-            print(f"{year}-{month1:02}-{day:02}")
-            break
-        else:
+        except ValueError:
             pass
+    except EOFError:
+        break
+
+    if (0<month1<=12) and (0<day<=31):
+        print(f"{year}-{month1:02}-{day:02}")
+        break
     else:
         pass
+
 
 
