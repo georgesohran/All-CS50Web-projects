e5f50275c7669f1f5282a0749396613a966b9a4b
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-09-18 16:19:18 +0000

    Mon, Sep 18, 2023, 7:19 PM +03:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -23,7 +23,7 @@ def format_check1(s):
 
 def format_check2(s):
     for m in month:
-        if s.startswith(m) and not s.has("/"):
+        if s.startswith(m) and s.has("/"):
             return True
     return False
 
