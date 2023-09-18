46335e0a98a38f6716db77a4c2166b622d49e812
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-09-18 11:24:11 +0000

    Mon, Sep 18, 2023, 2:24 PM +03:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -19,9 +19,16 @@ while True:
         ordate = input().replace(" ","")
         month,day,year = ordate.split("/")
         month,day,year = int(month),int(day),int(year)
-        print(f"{year}-{month:02}-{day:02}")
         break
     except ValueError:
         for m in month:
             if ordate.startswith(m):
                 month = month.index(m)+1
+                ordate.replace(m,"")
+                day,year = ordate.split(",")
+                break
+    except EOFError:
+        pass
+
+
+print(f"{year}-{month:02}-{day:02}")
