5eb956d2ea2cdbc0a0d9b5d7a5c4ba9227028735
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-09-18 10:57:17 +0000

    Mon, Sep 18, 2023, 1:57 PM +03:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -1,7 +1,9 @@
 
 
 while True:
-    ordate = input().replace(" ","")
-    month,day,year = ordate.split("/")
-    month,day,year = int(month),int(day),int(year)
-    print(f"{year}-{month:02}-{day:02}")
+    try:
+        ordate = input().replace(" ","")
+        month,day,year = ordate.split("/")
+        month,day,year = int(month),int(day),int(year)
+        print(f"{year}-{month:02}-{day:02}")
+    except ValueError
