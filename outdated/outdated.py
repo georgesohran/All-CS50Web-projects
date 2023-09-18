c57468a664d4a93c20ff24e328093d63a728f90d
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-09-18 12:36:32 +0000

    Mon, Sep 18, 2023, 3:36 PM +03:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -19,18 +19,16 @@ while True:
     try:
         month1,day,year = ordate.split("/")
         month1,day,year = int(month1),int(day),int(year)
-        month_name_check = month[month1]
     except ValueError:
         for m in month:
             if ordate.startswith(m):
-                month_name_check = m
                 month1 = month.index(m)+1
                 ordate = ordate.replace(m,'')
                 day,year = ordate.split(",")
                 day,year = int(day),int(year)
                 break
 
-    if (month_name_check in month) and (0<month1<=12) and (0<day<=31):
+    if (0<month1<=12) and (0<day<=31):
         break
     else:
         pass
