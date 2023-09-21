998512444f64011cb125f3f63b92e6032d85e54a
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-09-21 10:33:14 +0000

    Thu, Sep 21, 2023, 1:33 PM +03:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -6,9 +6,9 @@ figlet = Figlet()
 def main():
     word = input("Input:")
     if len(sys.argv) == 1:
-        font = random.choice(figlet.getFonts())
-        figlet.setFont(font=font)
-        figlet.renderText(word)
+        f = random.choice(figlet.getFonts())
+        figlet.setFont(font=f)
+        print(figlet.renderText(word))
 
     elif len(sys.argv) == 2:
         print("Invalid usage")
