139d61b7f9a6752bf87a62e6ed6ffb39c280aa16
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-09-21 10:49:11 +0000

    Thu, Sep 21, 2023, 1:49 PM +03:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -9,7 +9,7 @@ def main():
         sys.exit("Invalid usage")
     elif len(sys.argv) == 3:
         print(sys.argv)
-        if sys.argv[1] != '-f' or sys.argv[1] != '--font':
+        if sys.argv[1] != '-f' and sys.argv[1] != '--font':
             sys.exit("Invalid usage")
 
     if len(sys.argv) == 1:
