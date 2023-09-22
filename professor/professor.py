dfe3daf28fe47ea6fcbad9f627da7491ac389365
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-09-22 10:50:57 +0000

    Fri, Sep 22, 2023, 1:50 PM +03:00

--- professor/professor.py
+++ professor/professor.py
@@ -2,16 +2,22 @@ import random
 
 
 def main():
-    ...
+    pass
 
 
 def get_level():
-    ...
+    try:
+        lvl = int(input())
+        if lvl in range(3):
+            pass
+    except ValueError:
+        pass
 
 
 def generate_integer(level):
-    ...
+    pass
 
 
 if __name__ == "__main__":
     main()
+    for i in range
