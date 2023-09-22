e2e663956fb5b2f2a73625b52900fdd7ab5884f3
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-09-22 10:57:36 +0000

    Fri, Sep 22, 2023, 1:57 PM +03:00

--- professor/professor.py
+++ professor/professor.py
@@ -15,8 +15,12 @@ def get_level():
 
 
 def generate_integer(level):
-    pass
-
+    if level == 1:
+        return random.randint(0,10)
+    if level == 2:
+        return random.randint(11,100)
+    if level == 3:
+        return random.randint(101,1000)
 
 if __name__ == "__main__":
     main()
