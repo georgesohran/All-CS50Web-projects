ad7441a7bf98c64f5ec23e38060cc3565dc72f96
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-09-24 15:22:19 +0000

    Sun, Sep 24, 2023, 6:22 PM +03:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -2,9 +2,15 @@ import requests
 import sys
 
 try:
-    bitn = sys.argv[1]
+    try:
+        bitn = float(sys.argv[1])
+    except IndexError:
+        sys.exit("Missing command-line argument")
+    except ValueError:
+        sys.exit("Command-line argument is not a number")
+
     responce = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
     print(responce.json())
-    
+
 except requests.RequestException:
     pass
