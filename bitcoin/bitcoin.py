f42db36779b2d3b20c64f3e4aa23486e43222183
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-09-24 15:16:39 +0000

    Sun, Sep 24, 2023, 6:16 PM +03:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -5,5 +5,6 @@ try:
     bitn = sys.argv[1]
     responce = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
     print(responce.json())
+    
 except requests.RequestException:
-    ...
+    pass
