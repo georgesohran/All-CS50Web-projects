37c5e44a4de5c6fc760b0b458cf652c404cf46d3
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-09-26 14:40:17 +0000

    Tue, Sep 26, 2023, 5:40 PM +03:00

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -1,1 +1,14 @@
 from twttr import shorten
+
+def test_shorten_lower():
+    assert shorten("word") == "wrd"
+    assert shorten("twitter") == "twttr"
+    assert shorten("low") == "lw"
+
+def test_shorten_upper():
+    assert shorten("WORD") == "WRD"
+    assert shorten("TWITTER") == "TWTTR"
+    assert shorten("UPPER") == "PPR"
+
+def test_shorten_letters():
+    assert shorten("AEIOU n") == " n"
