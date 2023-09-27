43b5f991339d9cdf5c6462460f523a76646e7e6c
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-09-27 11:56:48 +0000

    Wed, Sep 27, 2023, 2:56 PM +03:00

---
+++ test_bank/bank.py
@@ -0,0 +1,21 @@
+
+def main():
+    g = input("Greeting: ")
+    g = g.lower().replace(" ","")
+
+
+
+def value(greeting):
+    ...
+
+
+if __name__ == "__main__":
+    main()
+
+
+if g.find("hello",0,5) != -1:
+    print("$0")
+elif g.find("h",0,1) != -1:
+    print("$20")
+else:
+    print("$100")
