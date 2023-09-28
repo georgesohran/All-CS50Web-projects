c1cdff6d5272986211bf63f1c4d84372fdff0641
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-09-28 10:20:33 +0000

    Thu, Sep 28, 2023, 1:20 PM +03:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -1,4 +1,14 @@
 from plates import is_valid
 
 def test_is_valid_cercount():
-    assert is_valid(AAA232)
+    assert is_valid("AAA232")
+    assert is_valid("ABCD")
+    assert not is_valid("ABCDEFG1")
+
+def test_is_valid_symbols():
+    assert not is_valid("ADA,!E")
+    assert not is_valid("!ALAW")
+
+def test_is_valid_last_cer():
+    assert not is_valid("ADA,!E")
+    assert not is_valid("!ALAW")
