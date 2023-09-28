791ae9dab45021d001c7670bd8dc640ef62ddd6e
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-09-28 10:24:56 +0000

    Thu, Sep 28, 2023, 1:24 PM +03:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -10,5 +10,15 @@ def test_is_valid_symbols():
     assert not is_valid("!ALAW")
 
 def test_is_valid_last_cer():
-    assert not is_valid("ADA,!E")
-    assert not is_valid("!ALAW")
+    assert is_valid("SAS21")
+    assert not is_valid("LIR23E")
+
+def test_is_valid_first_num():
+    assert is_valid("LRIP1")
+    assert not is_valid("LIR001")
+
+def test_is_valid_first_cer():
+    assert is_valid("CS50")
+    assert not is_valid("A123")
+
+
