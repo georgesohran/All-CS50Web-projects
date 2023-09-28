cdfbc822f292cf71f78da22a8b99f97870f699a6
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-09-28 11:30:15 +0000

    Thu, Sep 28, 2023, 2:30 PM +03:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -1,2 +1,15 @@
 from fuel import gauge, convert
 
+def test_gauge():
+    assert gauge(1) == "E"
+    assert gauge(100) == "F"
+    assert gauge(99) == "F"
+    assert gauge(27) == "27%"
+    assert gauge(51) == "51%"
+
+def test_convert():
+    assert convert(1) == "E"
+    assert convert(100) == "F"
+    assert convert(99) == "F"
+    assert convert(27) == "27%"
+    assert convert(51) == "51%"
