71aca99593a34716b19972452e98f06f67169f06
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-09-28 11:42:21 +0000

    Thu, Sep 28, 2023, 2:42 PM +03:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -8,8 +8,8 @@ def test_gauge():
     assert gauge(51) == "51%"
 
 def test_convert():
-    assert convert(13/10) == None
-    assert convert(12/0) == None
+    assert convert("13/10") == None
+    assert convert("12/0") == None
     assert convert("whaa") == None
-    assert convert(15/20) == 75
-    assert convert(99/100) == 99
+    assert convert("15/20") == 75
+    assert convert("99/100") == 99
