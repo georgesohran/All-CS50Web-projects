5e7480e4156c60837e2ad47e33b7a8ca44aa24cb
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-09-28 15:05:16 +0000

    Thu, Sep 28, 2023, 6:05 PM +03:00

--- test_fuel/fuel.py
+++ test_fuel/fuel.py
@@ -2,15 +2,12 @@ def main():
     while True:
         try:
             a = input("Fraction: ")
-            if convert(a) == None:
-                pass
+            a = convert(a)
+            if gauge(a) != None:
+                print(gauge(a))
+                break
             else:
-                a = convert(a)
-                if gauge(a) != None:
-                    print(gauge(a))
-                    break
-                else:
-                    pass
+                pass
         except (ValueError, ZeroDivisionError):
             pass
 
@@ -20,7 +17,8 @@ def convert(fraction):
     x, y = int(x), int(y)
     if y == 0:
         raise ZeroDivisionError
-    return int((x / y)*100)
+    else:
+        return int((x / y)*100)
 
 
 def gauge(percentage):
