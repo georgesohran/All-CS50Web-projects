5d49295a93be8d06cffd72edc70c53afd7852d02
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-09-28 15:12:37 +0000

    Thu, Sep 28, 2023, 6:12 PM +03:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -3,7 +3,6 @@ import pytest
 
 
 def test_gauge():
-    with pytest.raise
     assert gauge(124) == None
     assert gauge(1) == "E"
     assert gauge(100) == "F"
@@ -12,8 +11,10 @@ def test_gauge():
     assert gauge(51) == "51%"
 
 def test_convert():
-    assert convert("13/10") == 130
-    assert convert("12/0") == None
-    assert convert("whaa") == None
+    with pytest.raise(ZeroDivisionError):
+        assert convert("13/10")
+        assert convert("12/0")
+    with pytest.raise(ValueError):
+        assert convert("whaa")
     assert convert("15/20") == 75
     assert convert("99/100") == 99
