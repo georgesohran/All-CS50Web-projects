23bb46e6cbf620605968e1718a1dd7e39e8e6633
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-09-29 12:35:53 +0000

    Fri, Sep 29, 2023, 3:35 PM +03:00

--- lines/lines.py
+++ lines/lines.py
@@ -1,9 +1,14 @@
 import sys
 
 try:
-    if len(sys.argv)
+    if len(sys.argv)>2:
+        sys.exit("Too many command-line arguments")
+    if len(sys.argv)<1:
+        sys.exit("Too few command-line arguments")
+    
     f = sys.argv[1]
     with open(f) as File:
         lines = File.readlines():
 
-except
+except FileNotFoundError:
+    sys.exit("File does not exist")
