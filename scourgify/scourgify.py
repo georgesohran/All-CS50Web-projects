2f05df82cf905c51b7e9684ebd8c9b68b1359b5d
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-09-30 12:20:56 +0000

    Sat, Sep 30, 2023, 3:20 PM +03:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -1,20 +1,26 @@
 import sys
 import csv
 
+def main():
+    try:
+        check_input()
 
-try:
+        before = sys.argv[1]
+        after = sys.argv[2]
+
+        with open(before) as file:
+            reader = csv.reader(file)
+
+    except FileNotFoundError:
+        sys.exit(f"Could not read {sys.argv[1]}")
+
+def check_input():
     if len(sys.argv)>3:
         sys.exit("Too many command-line arguments")
     if len(sys.argv)<=2:
         sys.exit("Too few command-line arguments")
-    if not sys.argv[1].endswith(".csv"):
+    if not sys.argv[1].endswith(".csv") or not sys.argv[2].endswith(".csv"):
         sys.exit("Not a CSV file ")
 
-    before = sys.argv[1]
-    after = sys.argv[2]
 
-    with open(before) as file:
-        reader = csv.reader(file)
-
-except FileNotFoundError:
-    sys.exit(f"Could not read {sys.argv[1]}")
+main()
