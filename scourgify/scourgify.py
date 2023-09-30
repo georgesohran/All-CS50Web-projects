c3baa7051d37c5e219d3f38f989a833dca2b0786
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-09-30 12:13:50 +0000

    Sat, Sep 30, 2023, 3:13 PM +03:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -2,12 +2,10 @@ import sys
 import csv
 
 
-table = []
-
 try:
     if len(sys.argv)>3:
         sys.exit("Too many command-line arguments")
-    if len(sys.argv)<=1:
+    if len(sys.argv)<=2:
         sys.exit("Too few command-line arguments")
     if not sys.argv[1].endswith(".csv"):
         sys.exit("Not a CSV file ")
@@ -17,10 +15,6 @@ try:
     with open(p) as file:
         pizza_type = "Regular Pizza"
         reader = csv.reader(file)
-        for row in reader:
-            table.append(row)
-
-    print(tabulate(table[1:], table[0], tablefmt="grid"))
 
 except FileNotFoundError:
     sys.exit("File does not exist")
