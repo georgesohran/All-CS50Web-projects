0629064bcff9e804ba3a8b869b887220e36a17fe
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-09-30 07:43:52 +0000

    Sat, Sep 30, 2023, 10:43 AM +03:00

--- lines/lines.py
+++ lines/lines.py
@@ -5,11 +5,13 @@ try:
         sys.exit("Too many command-line arguments")
     if len(sys.argv)<1:
         sys.exit("Too few command-line arguments")
+    if not sys.argv[1].endswith(".py"):
+        sys.exit("Not a Python file ")
 
     f = sys.argv[1]
     with open(f) as File:
         lines = File.readlines()
-        
+
 
 except FileNotFoundError:
     sys.exit("File does not exist")
