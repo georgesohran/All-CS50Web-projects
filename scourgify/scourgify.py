fdbbec888e2e2a5ebbd41e2f83adc6e8ec8f96a9
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-09-30 12:39:50 +0000

    Sat, Sep 30, 2023, 3:39 PM +03:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -25,7 +25,7 @@ def check_input():
         sys.exit("Not a CSV file ")
 
 def seporate(row_dict):
-    full = row_dict.popitem()
+    full = row_dict.pop("name")
     row_dict["last"],row_dict["first"] = full.replace(" ","").split(",")
     print(row_dict)
 
