531bd287a0810d8a4f80c78790232d329061c30f
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-10-03 03:10:02 +0000

    Tue, Oct 3, 2023, 6:10 AM +03:00

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -1,4 +1,9 @@
 import numb3rs
 
 def test_validate():
-    assert 
+    assert numb3rs.validate("1.2.3.4")
+    assert numb3rs.validate("255.255.255.255")
+    assert numb3rs.validate("0.0.0.0")
+    assert not numb3rs.validate("1.2.3.999")
+    assert not numb3rs.validate("2.3.4")
+    assert not numb3rs.validate("4./1d34")
