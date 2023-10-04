0ebbebc05c309db5e547d18f4aad64d482117202
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-10-04 12:39:18 +0000

    Wed, Oct 4, 2023, 3:39 PM +03:00

--- working/working.py
+++ working/working.py
@@ -7,7 +7,7 @@ def main():
 
 
 def convert(s):
-    if match1 := re.search(r"([0-9]{1,2}\:?[0-9]{1,2}? (?:AM)?|(?:PM)?)",s  ):
+    if match1 := re.search(r"([0-9]{1,2}\:?[0-9]{1,2}? (AM)?|(PM)?) to ([0-9]{1,2}\:?[0-9]{1,2}? (AM)?|(PM)?)",s  ):
 
         return match1.group(1)
 
