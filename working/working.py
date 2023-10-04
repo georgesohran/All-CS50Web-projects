4cbd6f82ca7a9aa6a1e97f32f469f030ce27f2dc
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-10-04 15:33:07 +0000

    Wed, Oct 4, 2023, 6:33 PM +03:00

--- working/working.py
+++ working/working.py
@@ -8,15 +8,12 @@ def main():
 
 def convert(s):
     tfrom , tto = s.split(" to ")
-    match1 = re.search(r"([0-9]{1,2})\:?([0-9]{1,2})? (AM)?|(PM)?",tfrom)
-    match2 = re.search(r"([0-9]{1,2})\:?([0-9]{1,2})? (AM)?|(PM)?",tto)
+    match1 = re.search(r"([0-9]{1,2})\:?([0-9]{1,2})? (AM?|PM?)",tfrom)
+    match2 = re.search(r"([0-9]{1,2})\:?([0-9]{1,2})? (AM?|PM?)?",tto)
     if match1 and match2:
         hours1, mins1 = match1.group(1), match1.group(2)
-        if match1.group(3) == None :
-            APM1  = match1.group(4)
-        else:
-            APM1  = match1.group(3)
-        
+        APM1  = match1.group(3)
+
 
 
 
