1d0358601a2ef101821810d0beb9481c05c9ad58
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-10-04 15:51:43 +0000

    Wed, Oct 4, 2023, 6:51 PM +03:00

--- working/working.py
+++ working/working.py
@@ -11,11 +11,15 @@ def convert(s):
     match1 = re.search(r"([0-9]{1,2})\:?([0-9]{1,2})? (AM?|PM?)",tfrom)
     match2 = re.search(r"([0-9]{1,2})\:?([0-9]{1,2})? (AM?|PM?)",tto)
     if match1 and match2:
-        hours1, mins1 = match1.group(1), match1.group(2)
+        hours1, mins1 = int(match1.group(1)), int(match1.group(2))
         APM1  = match1.group(3)
-        hours2, mins2 = match2.group(1), match2.group(2)
-        APM2  = match1.group(3)
-        return "success"
+        hours2, mins2 = int(match2.group(1)), int(match2.group(2))
+        APM2  = match2.group(3)
+        if APM1 == "PM":
+            hours1 += 12
+        if APM2 == "PM":
+            hours2 += 12
+        return
 
 
 
