cde75dfdfd8449e07c9e319f1f1be347acc5d7ee
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-10-04 09:25:37 +0000

    Wed, Oct 4, 2023, 12:25 PM +03:00

--- watch/watch.py
+++ watch/watch.py
@@ -8,11 +8,13 @@ def main():
 def parse(s):
     mat = re.search(r"^\<iframe .*?src\=\"(.*?)\".*\<\/iframe\>$",s)
     if mat:
-        return mat.group(1)
+        r = reformat(mat.group(1))
+        return r
 
 def reformat(s):
     if s.find("youtube")!=-1:
-        
+        _,_,_,_,catch = s.split("/")
+        return "https://youtu.be/"+catch
 
 
 if __name__ == "__main__":
