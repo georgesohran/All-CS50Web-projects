235d58acec2c91040ad6c2117aaa381338bc7715
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-10-07 14:03:32 +0000

    Sat, Oct 7, 2023, 5:03 PM +03:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -1,12 +1,15 @@
 from datetime import deltatime
 from datetime import date
 import inflect as inf
+import re
 
 
 def main():
-    ...
-
-
+    input_date = input("Date of Birth:")
+    if match := re.search(r"([0-9]{1,4})-([0-9]{1,2})-([0-9]{1,2})")
+        print("cool")
+    else:
+        print("no")
 ...
 
 
