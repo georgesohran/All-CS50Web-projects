5ac0365e5cb0f4fb583c1b71a1bd6da243a3f4d4
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-10-07 14:19:20 +0000

    Sat, Oct 7, 2023, 5:19 PM +03:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -7,12 +7,9 @@ import re
 def main():
     input_date = input("Date of Birth:")
     if match := re.search(r"([0-9]{1,4})-([0-9]{1,2})-([0-9]{1,2})",input_date):
-        input_date = date()
-        input_date.year = match.group(1)
-        input_date.month = match.group(2)
-        input_date.day = match.group(3)
+        input_date = date(int(match.group(1)),int(match.group(2)),int(match.group(3)))
         current_date = date.today()
-        
+        print(input_date)
     else:
         print("no")
 ...
