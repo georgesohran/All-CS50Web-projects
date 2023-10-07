8310bffd2a21fc9017a0fc4d7f9e887c183fd1b8
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-10-07 14:50:28 +0000

    Sat, Oct 7, 2023, 5:50 PM +03:00

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -1,5 +1,7 @@
 from seasons import convert_date
 
 def test_convert_date():
+    assert convert_date("January 1, 1999",) == "Invalid date"
     assert convert_date("1999-01-01") == "Five hundred twenty-five thousand, six hundred minutes"
-    assert convert_date("")
+    assert convert_date("1999-12-31") == "One thousand, four hundred forty minutes"
+    assert convert_date("1970-01-01") == "Fifteen million, seven hundred seventy-eight thousand eighty minutes"
