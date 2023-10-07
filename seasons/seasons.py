0568e60f28eed07702b32248091c12d8870f33ab
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-10-07 14:28:34 +0000

    Sat, Oct 7, 2023, 5:28 PM +03:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -6,14 +6,17 @@ import re
 
 def main():
     input_date = input("Date of Birth:")
-    if match := re.search(r"([0-9]{1,4})-([0-9]{1,2})-([0-9]{1,2})",input_date):
-        input_date = date(int(match.group(1)),int(match.group(2)),int(match.group(3)))
-        current_date = date.today()
-        time_passed_date = current_date - input_date
-        total_mins = time_passed_date.total_seconds()/60
-        print(total_mins)
-    else:
-        print("no")
+    try:
+        if match := re.search(r"([0-9]{1,4})-([0-9]{1,2})-([0-9]{1,2})",input_date):
+            input_date = date(int(match.group(1)),int(match.group(2)),int(match.group(3)))
+            current_date = date.today()
+            time_passed_date = current_date - input_date
+            total_mins = time_passed_date.total_seconds()/60
+            print(total_mins)
+        else:
+            print("Invalid date")
+    except ValueError:
+        print("Invalid date")
 ...
 
 
