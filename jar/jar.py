2c5fecded7d28017164b700261428f90c33e7d24
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-10-07 16:04:33 +0000

    Sat, Oct 7, 2023, 7:04 PM +03:00

--- jar/jar.py
+++ jar/jar.py
@@ -1,15 +1,20 @@
 class Jar:
     def __init__(self, capacity=12):
         self.capacity = capacity
+        if self.capacity < 0:
+            raise ValueError
+        self.size = 0
 
     def __str__(self):
-        ...
+        return "🍪"*self.size
 
     def deposit(self, n):
-        ...
+        self.size += n
 
     def withdraw(self, n):
-        ...
+        if self.size - n < 0:
+            raise ValueError
+        self.size -= n
 
     @property
     def capacity(self):
@@ -17,10 +22,5 @@ class Jar:
 
     @property
     def size(self):
-        ...
+        return self._size
 
-    @capacity.setter
-    def capacity(self,capacity):
-        if self._capacity < 0:
-            raise ValueError
-        self._capacity = capacity
