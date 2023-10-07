efbb14c691e297c96830558b313d67e96d1e44cd
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-10-07 14:36:02 +0000

    Sat, Oct 7, 2023, 5:36 PM +03:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -14,12 +14,26 @@ def main():
             time_passed_date = current_date - input_date
             total_mins = time_passed_date.total_seconds()/60
             word = p.number_to_words(int(total_mins), andword="")
-            print(word)
+            print(word,"minutes")
         else:
             print("Invalid date")
     except ValueError:
         print("Invalid date")
-...
+
+def convert_date(d):
+    try:
+        if match := re.search(r"([0-9]{1,4})-([0-9]{1,2})-([0-9]{1,2})",input_date):
+            input_date = date(int(match.group(1)),int(match.group(2)),int(match.group(3)))
+            current_date = date.today()
+            time_passed_date = current_date - input_date
+            total_mins = time_passed_date.total_seconds()/60
+            word = p.number_to_words(int(total_mins), andword="")
+            print(word,"minutes")
+        else:
+            print("Invalid date")
+    except ValueError:
+        print("Invalid date")
+
 
 
 if __name__ == "__main__":
