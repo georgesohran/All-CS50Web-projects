a198583720ff306d790b80a76c95975ab4b0177a
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-10-08 12:17:19 +0000

    Sun, Oct 8, 2023, 3:17 PM +03:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -4,14 +4,14 @@ from PIL import Image
 class PDF(FPDF):
     def __innit__(self):
         self._pdf = FPDF()
-        self._pdf.image("shirtificate.png", w = _pdf.pdf.epw)
+
 
 
 
     def header(self):
         self.set_font("helvetica", "B", 30)
         self.cell(30)
-        # Printing title:
+        self.image("shirtificate.png", w = self.epw)
         self.cell(30, 10, "Title", align="C")
 
 
