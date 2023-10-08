47d9ba30c3b4b858d7cd8881c9baff8fb5d29ff8
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-10-08 11:39:03 +0000

    Sun, Oct 8, 2023, 2:39 PM +03:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -23,7 +23,7 @@ class PDF(FPDF):
 def main():
     pdf = PDF()
     pdf.add_page()
-    pdf.image("shirtificate.png", 10, 8, 33)
+    pdf.image("shirtificate.png", 0, 0, 33)
     pdf.set_title("CS50 Shirtificate")
     pdf.output("test.pdf")
 
