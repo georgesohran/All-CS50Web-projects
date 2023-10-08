51f71f84da255d3fb799b8feb301be0b2e6d872a
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-10-08 11:53:19 +0000

    Sun, Oct 8, 2023, 2:53 PM +03:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -9,16 +9,8 @@ class PDF(FPDF):
         self.cell(30)
         # Printing title:
         self.cell(30, 10, "Title", border=1, align="C")
-        # Performing a line break:
-        self.ln(20)
 
-    def footer(self):
-        # Position cursor at 1.5 cm from bottom:
-        self.set_y(-15)
-        # Setting font: helvetica italic 8
-        self.set_font("helvetica", "I", 8)
-        # Printing page number:
-        self.cell(0, 10, f"Page {self.page_no()}/{{nb}}", align="C")
+
 
 
 def main():
