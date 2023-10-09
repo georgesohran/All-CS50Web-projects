55ef95e4a5e004debb0cad3ffee8e2d9e07ffeb6
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-10-09 10:45:55 +0000

    Mon, Oct 9, 2023, 1:45 PM +03:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -5,6 +5,22 @@ class PDF(FPDF):
     def __innit__(self):
         super().__innit__()
 
+    def header(self):
+        self.set_font("helvetica", "B", 15)
+        width = self.get_string_width(self.title) + 6
+        self.set_x((210 - width) / 2)
+        self.cell(
+            width,
+            9,
+            self.title,
+            border=1,
+            new_x="LMARGIN",
+            new_y="NEXT",
+            align="C",
+            fill=True,
+        )
+        self.ln(10)
+
 
 def main():
     pdf = PDF()
@@ -12,7 +28,6 @@ def main():
     pdf.add_page()
     pdf.set_title("CS50 Shirtificate")
     pdf.image("shirtificate.png", w = pdf.epw)
-
     pdf.output("test.pdf")
 
 main()
