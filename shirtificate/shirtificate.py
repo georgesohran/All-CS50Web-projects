23f4df23161645047761d9f631fbf00d2673fb65
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-10-09 11:46:37 +0000

    Mon, Oct 9, 2023, 2:46 PM +03:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -3,13 +3,6 @@ from PIL import Image
 
 class PDF(FPDF):
     text_hight = 30
-    def __init__(self,name):
-        self.image("shirtificate.png", w = 210)
-        self.set_font("helvetica", "B", 20)
-        self.text_color(255,255,255)
-        self.text(self.epw/2, self.text_hight*3, f"{name}took CS50")
-
-
     def header(self):
         self.set_font("helvetica", "B", 36)
         self.set_x(210 / 2)
@@ -23,18 +16,21 @@ class PDF(FPDF):
         )
         self.ln(10)
 
-
-
-
+    def render_text(self,txt):
+        self.image("shirtificate.png", w = self.epw)
+        self.set_font("helvetica", "B", 20)
+        self.set_text_color(255,255,255)
+        self.text(self.epw/2, self.text_hight*3, f"{txt}took CS50")
 
 
 
 
 def main():
     name = input("Name: ")
-    pdf = PDF(name)
+    pdf = PDF()
     pdf.set_margin(0)
     pdf.add_page()
+    pdf.render_text(name)
     pdf.output("test.pdf")
 
 main()
