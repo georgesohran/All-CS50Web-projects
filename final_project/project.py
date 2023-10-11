c89e261aefccf924d8a12cc7e61236a0310e8d6e
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-10-11 13:03:41 +0000

    Wed, Oct 11, 2023, 4:03 PM +03:00

--- final_project/project.py
+++ final_project/project.py
@@ -9,9 +9,9 @@ def main():
     main_window = Tk()
     main_window.title("math.py")
     main_window.geometry("1200x600")
-    canvas = Canvas(main_window, bg="blue", height=250, width=300)
+    canvas = Canvas(main_window, bg="blue", height=600, width=600)
     canvas.create_line(15, 25, 200, 25)
-    main_window.mainloop()
+    mainloop()
 
 
 if __name__ == "__main__":
