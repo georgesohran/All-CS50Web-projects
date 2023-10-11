7aca499efd97c85d274189fbd2b78f594591ba80
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-10-11 12:38:37 +0000

    Wed, Oct 11, 2023, 3:38 PM +03:00

--- final_project/project.py
+++ final_project/project.py
@@ -7,6 +7,11 @@ import csv
 
 
 def main():
-    main_window = Tk(screenName="math.py")
-    
+    main_window = Tk()
+    main_window.geometry('1370x700')
     canvas = Canvas(main_window)
+    main_window.mainloop()
+
+
+if __name__ == "__main__":
+    main()
