2530b6546a1eafca36038461f6bb865df0d1182b
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-10-11 15:52:49 +0000

    Wed, Oct 11, 2023, 6:52 PM +03:00

--- final_project/project.py
+++ final_project/project.py
@@ -1,18 +1,26 @@
 from tkinter import *
 from tkinter import ttk
 
-import sys
-import csv
+class Cords(Canvas):
+    def __init__(self):
+        pass
+
+
 
 
 def main():
     main_window = Tk()
     main_window.title("math.py")
     main_window.geometry("1200x600")
-    canvas = Canvas(main_window, bg="blue", height=600, width=600)
+    canvas = Canvas(main_window, bg="#C0C0C0", height=600, width=600)
+    canvas.pack(anchor=W)
     canvas.create_line(15, 25, 200, 25)
     canvas.create_line(15, 25, 40, 25)
     main_window.mainloop()
+
+def generate(func):
+    for x in range(-50,50):
+        pass
 
 
 if __name__ == "__main__":
