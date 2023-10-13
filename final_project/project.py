554ab329a6cfbd13c7cc84dd0d7aae17c4aa09cb
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-10-13 08:56:08 +0000

    Fri, Oct 13, 2023, 11:56 AM +03:00

--- final_project/project.py
+++ final_project/project.py
@@ -1,10 +1,12 @@
 from tkinter import *
-from tkinter import ttk
+from numpy import arange
+import math
 
-class Cords(Canvas):
-    def __init__(self):
-        pass
+graph_size = 600
 
+#offset of the coordinate system
+x_rel = graph_size/2
+y_rel = graph_size/2
 
 
 
@@ -12,15 +14,47 @@ def main():
     main_window = Tk()
     main_window.title("math.py")
     main_window.geometry("1200x600")
-    canvas = Canvas(main_window, bg="#C0C0C0", height=600, width=600)
-    canvas.pack(anchor=W)
-    canvas.create_line(15, 25, 200, 25)
-    canvas.create_line(15, 25, 40, 25)
+
+    canvas = Canvas(main_window, bg="#C0C0C0", height=graph_size, width=graph_size)
+    canvas.place(x=0,y=0)
+
+    #points of a graph
+    x1 = -100
+    y1 = f(x1)
+
+    canvas.create_line(graph_size/2 , 0 , graph_size/2 , graph_size , width=3)
+    canvas.create_line(0 , graph_size/2 ,graph_size , graph_size/2, width=3)
+
+    zoom_step = 20
+    x_step = 0.05
+    for x in arange(-100,100,x_step):
+        try:
+            y = f(x)
+            if not (y1 - y < -50) and not (y1 - y > 50):
+                canvas.create_line(
+                                    (x1*zoom_step+x_rel),
+                                    (y1*zoom_step+y_rel),
+                                    (x*zoom_step+x_rel),
+                                    (y*zoom_step+y_rel),
+                                    width = 2
+                                    )
+            else:
+                pass
+            x1,y1 = x , y
+        except ZeroDivisionError:
+            x1 += 2
+            y1 = f(x1)
+
+
     main_window.mainloop()
 
-def generate(func):
-    for x in range(-50,50):
-        pass
+
+
+def f(x):
+    return -(math.tan(x))
+
+
+
 
 
 if __name__ == "__main__":
