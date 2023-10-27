3e7bb4befee43983d744adc5e6a47f9776a4b969
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-10-27 15:52:13 +0000

    Fri, Oct 27, 2023, 6:52 PM +03:00

---
+++ mario-more/mario.c
@@ -0,0 +1,26 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    //gEtTinG HeIghT FroM ThE USer
+    int height
+
+    do
+    {
+        height = get_int("Height:")
+    }
+    while((hight < 1) or (height > 8))
+
+    int count = 0
+
+    while(count < height)
+    {
+        count ++
+        for(count , )
+        {
+            printf(" ")
+        }
+
+    }
+}
