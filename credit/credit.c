12c17195523ff36df1d5453f5fc539a6f198f298
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-10-29 12:18:12 +0000

    Sun, Oct 29, 2023, 3:18 PM +03:00

--- credit/credit.c
+++ credit/credit.c
@@ -11,29 +11,6 @@ int main(void)
 
     int second_sum = 0;
 
-    for(int power = 0; power >= 16; power++)
-    {
-        if (power % 2 == 0)
-        {
-            long x = n % (long) pow(10 , power);
-            // - n % (long) pow(10 , power-1)
-            if ((10 < x) & (x > 100))
-            {
-                first_sum += x % 10;
-                first_sum += x % 100 //- x % 10;
-            }
-
-            else
-            {
-                first_sum += x;
-            }
-        }
-        else
-        {
-            second_sum += n % (long) pow(10 , power);
-        }
-    }
-
     int final_sum = second_sum + first_sum;
 
     if (final_sum % 10 == 0)
