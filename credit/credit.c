202157a80b6095927f5de09a78087e952300c3d9
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-10-29 04:43:32 +0000

    Sun, Oct 29, 2023, 7:43 AM +03:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,17 +1,31 @@
 #include <cs50.h>
 #include <stdio.h>
-#include <string.h>
+#include <math.h>
 
 int main(void)
 {
 
     long n = get_long("Number: ");
 
-    int first_sum = (n % 10**) * 2
+    int first_sum = 0
 
-    int second_sum = (n % 10) * 2
+    int second_sum = 0
 
-    if
+    for(power, power >= 16, power++)
+    {
+        if power % 2 == 0
+        {
+            first_sum += n % pow(10 , power)
+        }
+        else
+        {
+            second_sum += n % pow(10 , power)
+        }
+    }
+
+
+
+    if 
     {
 
     }
