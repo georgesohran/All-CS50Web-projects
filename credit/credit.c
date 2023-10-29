6381b7f52bfe958f430cb3e4b88961eee7fc161a
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-10-29 12:23:56 +0000

    Sun, Oct 29, 2023, 3:23 PM +03:00

--- credit/credit.c
+++ credit/credit.c
@@ -16,11 +16,11 @@ int main(void)
         if (power % 2 == 0)
         {
             long x = n % (long) pow(10 , power);
-            // - n % (long) pow(10 , power-1)
+
             if ((10 < x) & (x > 100))
             {
                 first_sum += x % 10;
-                first_sum += x % 100; //- x % 10;
+                first_sum += x % 100; 
             }
 
             else
