01e0d569a89c968774aa23dbc6c44198476bf458
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-10-31 11:14:57 +0000

    Tue, Oct 31, 2023, 2:14 PM +03:00

--- credit/credit.c
+++ credit/credit.c
@@ -10,12 +10,12 @@ int main(void)
 
     int second_sum = 0;
 
-    for (int power1 = 1; power1 <= 8; power1 += 2)
+    for (int power1 = 1; power1 <= 16; power1 += 2)
     {
         second_sum += n % (long) pow(10, power1) / (long) pow(10, (power1 - 1));
     }
 
-    for (int power2 = 2; power2 <= 8; power2 += 2)
+    for (int power2 = 2; power2 <= 16; power2 += 2)
     {
         int x = (n % (long) pow(10, power2) / (long) pow(10, (power2 - 1))) * 2;
 
