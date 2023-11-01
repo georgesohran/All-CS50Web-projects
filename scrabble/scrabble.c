fb8fbd36787ece507b188a7c05c82cc399c261d6
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-11-01 11:44:38 +0000

    Wed, Nov 1, 2023, 2:44 PM +03:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -21,15 +21,15 @@ int main(void)
     // TODO: Print the winner
     if (score1 > score2)
     {
-        printf("Player 1 wins !");
+        printf("Player 1 wins !\n");
     }
     else if(score1 < score2)
     {
-        printf("Player 2 wins !");
+        printf("Player 2 wins !\n");
     }
     else
     {
-        printf("Tie !/");
+        printf("Tie !\n");
     }
 
 }
