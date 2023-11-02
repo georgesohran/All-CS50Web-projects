771e0b3418f3f62461ab28a076a2e33eac077a1f
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-11-02 11:07:29 +0000

    Thu, Nov 2, 2023, 2:07 PM +03:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,21 +1,21 @@
 #include <cs50.h>
 #include <stdio.h>
-#include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
     string text = get_string("Text:");
 
-    grade = get_grade(text)
+    grade = get_grade(text);
 
 }
 
 int get_grade(string txt)
 {
-    int cer_count = 0
-    int word_count = 0
-    int sentence_count = 0
+    int cer_count = 0;
+    int word_count = 0;
+    int sentence_count = 0;
 
-    for
+    for (int i; i < strlen(txt))
 
 }
