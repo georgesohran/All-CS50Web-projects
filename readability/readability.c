a76e856592d25509018c8c979e3a081ff5c5dad2
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-11-03 10:26:30 +0000

    Fri, Nov 3, 2023, 1:26 PM +03:00

--- readability/readability.c
+++ readability/readability.c
@@ -16,6 +16,9 @@ int main(void)
     int word_count = count_words(text);
     int sentence_count = count_sentances(text);
 
+    float L = ((float) letter_count)
+    float S
+
     printf("%i %i %i \n", letter_count, word_count, sentence_count);
 
 }
@@ -42,7 +45,7 @@ int count_words(string txt)
     for(int i = 0; i < strlen(txt); i++)
     {
         if (!(txt[i] >= 97 && txt[i] <= 122) && !(txt[i] >= 65 && txt[i] <= 90) &&
-            !(txt[i] == 96) && !(txt[i] == 45) &&
+            !(txt[i] == 39) && !(txt[i] == 45) &&
             ((txt[i-1] >= 97 && txt[i-1] <= 122) || (txt[i-1] >= 65 && txt[i-1] <= 90)))
         {
             count ++;
