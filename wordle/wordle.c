59b1ca7f8fd98f9de391aa62fd05a5eb053d039b
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-11-03 16:07:31 +0000

    Fri, Nov 3, 2023, 7:07 PM +03:00

--- wordle/wordle.c
+++ wordle/wordle.c
@@ -90,11 +90,7 @@ int main(int argc, string argv[])
         // set all elements of status array initially to 0, aka WRONG
         // TODO #4
 
-        do
-        {
-
-        }
-        while()
+        
 
         // Calculate score for the guess
         int score = check_word(guess, wordsize, status, choice);
