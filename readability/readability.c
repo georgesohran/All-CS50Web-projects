db3d8a2221f099b3aee9c8e530dd8417720586ff
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-11-03 10:19:09 +0000

    Fri, Nov 3, 2023, 1:19 PM +03:00

--- readability/readability.c
+++ readability/readability.c
@@ -41,7 +41,7 @@ int count_words(string txt)
 
     for(int i = 0; i < strlen(txt); i++)
     {
-        if (!(txt[i] >= 97 && txt[i] <= 122) && !(txt[i] >= 65 && txt[i] <= 90) && !(txt[i] == 96)
+        if (!(txt[i] >= 97 && txt[i] <= 122) && !(txt[i] >= 65 && txt[i] <= 90) &&
             ((txt[i-1] >= 97 && txt[i-1] <= 122) || (txt[i-1] >= 65 && txt[i-1] <= 90)))
         {
             count ++;
