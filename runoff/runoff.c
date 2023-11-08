5e4966501af1e6d413d091e7ba061dfdfb6dec4a
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-11-08 09:37:56 +0000

    Wed, Nov 8, 2023, 12:37 PM +03:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -128,9 +128,11 @@ bool vote(int voter, int rank, string name)
 {
     for (int i = 0; i < voter_count; i++)
     {
-        if (strcmp(name, candidates[i]) == 0)
+        if (strcmp(name, candidates[i].name) == 0)
         {
-            
+
+
+            return true;
         }
     }
     return false;
