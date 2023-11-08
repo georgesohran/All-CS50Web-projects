0938b24b40a1b20862fd009c24ab47a9134e8d3b
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-11-08 10:21:13 +0000

    Wed, Nov 8, 2023, 1:21 PM +03:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -154,7 +154,10 @@ void tabulate(void)
 // Print the winner of the election, if there is one
 bool print_winner(void)
 {
-    // TODO
+    for(int i = 0; i < candidate_count; i++)
+    {
+        
+    }
     return false;
 }
 
