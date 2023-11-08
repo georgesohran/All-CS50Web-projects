00c475a75923dd61071c2bb454455d7158d36519
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-11-08 10:23:59 +0000

    Wed, Nov 8, 2023, 1:23 PM +03:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -154,9 +154,15 @@ void tabulate(void)
 // Print the winner of the election, if there is one
 bool print_winner(void)
 {
+    int half = voter_count / 2
+
     for(int i = 0; i < candidate_count; i++)
     {
-        
+        if (!candidate[i].eliminated && candidate[i].votes >= half)
+        {
+            print()
+        }
+
     }
     return false;
 }
