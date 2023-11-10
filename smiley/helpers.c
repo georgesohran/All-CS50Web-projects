e58322a1dec88384b8d066e812f55a2486bc8515
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-11-10 09:37:36 +0000

    Fri, Nov 10, 2023, 12:37 PM +03:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -2,5 +2,11 @@
 
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
-    // Change all black pixels to a color of your choosing
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j]
+        }
+    }
 }
