d7a203cf5e54d41de9ee18a14be2ba299b57bc1e
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-11-11 16:17:33 +0000

    Sat, Nov 11, 2023, 7:17 PM +03:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -40,13 +40,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < width; j++)
-        {
-            
-        }
-    }
+    
     return;
 }
 
