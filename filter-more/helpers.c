a0d24a395ab930dc4266f51e1ca8a64915f79b5e
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-11-11 10:05:10 +0000

    Sat, Nov 11, 2023, 1:05 PM +03:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -19,7 +19,13 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
-    
+    for (int i = 0; i < width; i++)
+    {
+        for (int j = 0; j < height; j++)
+        {
+            
+        }
+    }
     return;
 }
 
