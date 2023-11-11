76f0290fe77159217e3f607a6ec570f6d700237e
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-11-11 16:37:49 +0000

    Sat, Nov 11, 2023, 7:37 PM +03:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -40,11 +40,86 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    for (int i = 0; i < width/2; i++)
+    for (int i = 0; i < height; i++)
     {
-        for (int j = 0; j < height; j++)
+        for (int j = 0; j < width; j++)
         {
-            
+            long red_sum = 0;
+            long blue_sum = 0;
+            long green_sum = 0;
+
+            if (i == 0 && j == 0)
+            {
+                red_sum += image[i][j].rgbtRed;
+                red_sum += image[i+1][j].rgbtRed;
+                red_sum += image[i][j+1].rgbtRed;
+                red_sum += image[i+1][j+1].rgbtRed;
+            }
+            else if (i == 0 && j == width)
+            {
+                red_sum += image[i][j].rgbtRed;
+                red_sum += image[i][j-1].rgbtRed;
+                red_sum += image[i+1][j-1].rgbtRed;
+                red_sum += image[i+1][j].rgbtRed;
+            }
+            else if (i == height && j == 0)
+            {
+                red_sum += image[i][j].rgbtRed;
+                red_sum += image[i-1][j].rgbtRed;
+                red_sum += image[i-1][j+1].rgbtRed;
+                red_sum += image[i][j+1].rgbtRed;
+            }
+            else if (i == height && j == width)
+            {
+                red_sum += image[i][j].rgbtRed;
+                red_sum += image[i-1][j-1].rgbtRed;
+                red_sum += image[i-1][j].rgbtRed;
+                red_sum += image[i][j-1].rgbtRed;
+            }
+            else if (i == 0 && j < 0 && j > 0)
+            else if (i == width && j < 0 && j > 0)
+
+            red_sum += image[i][j].rgbtRed;
+            red_sum += image[i-1][j-1].rgbtRed;
+            red_sum += image[i-1][j].rgbtRed;
+            red_sum += image[i][j-1].rgbtRed;
+            red_sum += image[i-1][j+1].rgbtRed;
+            red_sum += image[i+1][j-1].rgbtRed;
+            red_sum += image[i+1][j].rgbtRed;
+            red_sum += image[i][j+1].rgbtRed;
+            red_sum += image[i+1][j+1].rgbtRed;
+
+
+
+            blue_sum += image[i][j].rgbtBlue;
+            blue_sum += image[i-1][j-1].rgbtBlue;
+            blue_sum += image[i-1][j].rgbtBlue;
+            blue_sum += image[i][j-1].rgbtBlue;
+            blue_sum += image[i-1][j+1].rgbtBlue;
+            blue_sum += image[i+1][j-1].rgbtBlue;
+            blue_sum += image[i+1][j].rgbtBlue;
+            blue_sum += image[i][j+1].rgbtBlue;
+            blue_sum += image[i+1][j+1].rgbtBlue;
+
+
+
+            green_sum += image[i][j].rgbtGreen;
+            green_sum += image[i-1][j-1].rgbtGreen;
+            green_sum += image[i-1][j].rgbtGreen;
+            green_sum += image[i][j-1].rgbtGreen;
+            green_sum += image[i-1][j+1].rgbtGreen;
+            green_sum += image[i+1][j-1].rgbtGreen;
+            green_sum += image[i+1][j].rgbtGreen;
+            green_sum += image[i][j+1].rgbtGreen;
+            green_sum += image[i+1][j+1].rgbtGreen;
+
+            int red_averege = red_sum / 9;
+            int green_averege = green_sum / 9;
+            int blue_averege = blue_sum / 9;
+
+            image[i][j].rgbtRed = red_averege;
+            image[i][j].rgbtBlue = blue_averege;
+            image[i][j].rgbtGreen = green_averege;
         }
     }
     return;
