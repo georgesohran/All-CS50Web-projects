4c758b32f4fb6ca25db26e5b7d15343b87cb8642
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-11-13 15:33:54 +0000

    Mon, Nov 13, 2023, 6:33 PM +03:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -254,9 +254,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     return;
 }
 
-int Gx[][] = [[-1,0,1],
-              [-2,0,2],
-              [-1,0,1]];
+int Gx[][] = [[-1,0,1],[-2,0,2],[-1,0,1]];
 
 int Gy[][] = [[-1,-2,-1],
               [0,0,0],
