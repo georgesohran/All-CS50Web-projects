11c8b1af082b53371c15f1a470ac2a45300a3403
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-11-19 11:23:15 +0000

    Sun, Nov 19, 2023, 2:23 PM +03:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -53,16 +53,20 @@ person *create_family(int generations)
         current_person.parents[1] = parent1;
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
-        current_person.alleles[0] = random_allale()
-        current_person.alleles[1] = random_allale()
+        current_person.alleles[0] = random_allale();
+        current_person.alleles[1] = random_allale();
     }
 
     // If there are no generations left to create
     else
     {
         // TODO: Set parent pointers to NULL
+        person.parents[0] = NULL;
+        person.parents[1] = NULL;
 
         // TODO: Randomly assign alleles
+        current_person.alleles[0] = random_allale();
+        current_person.alleles[1] = random_allale();
     }
 
     // TODO: Return newly created person
