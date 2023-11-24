0d73750fa8ce88db289ee7ba74e968fcc37bca81
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-11-24 16:20:49 +0000

    Fri, Nov 24, 2023, 7:20 PM +03:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -16,7 +16,8 @@ def main():
 
     teams = []
     # TODO: Read teams into memory from file
-    reader = csv.DictReader(sys.argv[1])
+    file = open(sys.argv[1])
+    reader = csv.DictReader(file)
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
