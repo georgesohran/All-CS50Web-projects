b71b8e458ab989099471944ef18e6d0b97912659
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-11-25 15:06:11 +0000

    Sat, Nov 25, 2023, 6:06 PM +03:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -4,9 +4,9 @@ from re import search
 def main():
     card_num = input("Number: ")
 
-    digcount = len(card_num)
+    if search("^14",card_num)
 
-def is_AMEX(card_num:str):
+def is_valid(card_num:str):
     sum1 = 0
     sum2 = 0
 
@@ -16,6 +16,11 @@ def is_AMEX(card_num:str):
             sum1 += (int(card_num[i])*2) % 100
         else:
             sum1 += int(card_num[i])*2
+
+    for i in range(len(card_num), step = 2, start = 1):
+        sum2 += card_num[i]
+
+    sum3 = sum1 + sum2
 
     return True
 
