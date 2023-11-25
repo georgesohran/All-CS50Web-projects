e7f2ec6a3be7052c56aa4408aa2fc4b9aa3fc8f3
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-11-25 15:19:13 +0000

    Sat, Nov 25, 2023, 6:19 PM +03:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -6,19 +6,19 @@ def main():
 
     digit_num = len(card_num)
 
-    if search(r"^4{0,9}*",card_num) and digit_num == 16:
+    if search(r"^4[0-9]*",card_num) and digit_num == 16:
         if is_valid(card_num):
             print("VISA")
         else:
             print("INVALID")
 
-    elif search(r"^5{0,9}*",card_num) and digit_num == 15:
+    elif search(r"^5[0-9]*",card_num) and digit_num == 15:
         if is_valid(card_num):
             print("MASTERCARD")
         else:
             print("INVALID")
 
-    elif search(r"^3{0,9}*",card_num) and digit_num == 15:
+    elif search(r"^3[0-9]*",card_num) and digit_num == 15:
         if is_valid(card_num):
             print("AMEX")
         else:
@@ -30,15 +30,15 @@ def is_valid(card_num:str):
     sum1 = 0
     sum2 = 0
 
-    for i in range(len(card_num), step = 2):
+    for i in range(0,len(card_num),2):
         if int(card_num[i])*2 > 9:
             sum1 += (int(card_num[i])*2) % 10
             sum1 += (int(card_num[i])*2) % 100
         else:
             sum1 += int(card_num[i])*2
 
-    for i in range(len(card_num), step = 2, start = 1):
-        sum2 += card_num[i]
+    for i in range(1,len(card_num),2):
+        sum2 += int(card_num[i])
 
     sum3 = sum1 + sum2
 
