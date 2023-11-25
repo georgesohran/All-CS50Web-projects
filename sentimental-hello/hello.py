29a1d7dea4e8b8cbbee772c86416130211f8992e
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-11-25 14:24:52 +0000

    Sat, Nov 25, 2023, 5:24 PM +03:00

--- sentimental-hello/hello.py
+++
@@ -1,6 +0,0 @@
-def main():
-    name = str(input("What is your name?\n"))
-    print(f"hello,{name}")
-
-if __name__ == "__main__":
-    main()
---
+++ sentimental-mario-more/mario.py
@@ -0,0 +1,9 @@
+def main():
+    height = int(input("Height"))
+
+    
+
+
+
+if __name__ == "__main__":
+    main()
