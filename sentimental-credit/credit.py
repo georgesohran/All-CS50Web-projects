84b060b3918ecc9cfa2ae0f818f647e034ec0dd1
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-11-25 15:22:47 +0000

    Sat, Nov 25, 2023, 6:22 PM +03:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -31,11 +31,12 @@ def is_valid(card_num:str):
     sum2 = 0
 
     for i in range(0,len(card_num),2):
+        num = int(card_num[i])*2
         if int(card_num[i])*2 > 9:
-            sum1 += (int(card_num[i])*2) % 10
-            sum1 += (int(card_num[i])*2) % 100
+            sum1 += num % 10
+            sum1 += num % 100
         else:
-            sum1 += int(card_num[i])*2
+            sum1 += num*2
 
     for i in range(1,len(card_num),2):
         sum2 += int(card_num[i])
