db33c75b7347516d5fcb2ab2e9b5c33886f524a0
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-11-25 14:52:54 +0000

    Sat, Nov 25, 2023, 5:52 PM +03:00

---
+++ sentimental-credit/credit.py
@@ -0,0 +1,15 @@
+import re
+
+
+def main():
+    card_num = input("Number: ")
+
+    digcount = len(card_num)
+
+def is_AMEX(card_num:str):
+    return True
+
+
+
+if __name__ == "__main__":
+    main()
