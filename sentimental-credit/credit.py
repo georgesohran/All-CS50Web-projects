5a614bcba218431994bffb37202d108b7b984d5d
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-11-25 15:00:08 +0000

    Sat, Nov 25, 2023, 6:00 PM +03:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,4 +1,4 @@
-import re
+from re import search
 
 
 def main():
@@ -10,7 +10,9 @@ def is_AMEX(card_num:str):
     sum1 = 0
 
     for i in range(len(card_num), step = 2):
-        sum1 += int(card_num[i])
+        if int(card_num[i])*2 > 9:
+            sum1
+        sum1 += int(card_num[i])*2
 
     return True
 
