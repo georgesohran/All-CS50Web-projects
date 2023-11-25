d279ad65f834055936ac7be7897558c1bb601a7c
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-11-25 14:34:03 +0000

    Sat, Nov 25, 2023, 5:34 PM +03:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,8 +1,21 @@
 def main():
-    height = int(input("Height"))
+    height = -48
+    while height <= 0:
+        try:
+            height = int(input("Height: "))
+        except ValueError:
+            pass
 
-    
+    for i in range(height):
+        print_row(i+1,height)
 
+
+
+def print_row(i,h):
+    print(" " * (h-i), end="")
+    print("#" * i, end="")
+    print("  ", end="")
+    print("#" * i)
 
 
 if __name__ == "__main__":
