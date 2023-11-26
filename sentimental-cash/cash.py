e6e281afffc6f9307c3a3d57675205978f38faa3
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-11-26 15:14:26 +0000

    Sun, Nov 26, 2023, 6:14 PM +03:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,1 +1,18 @@
-# TODO
+import re
+
+def main():
+
+    change = get_input()
+
+    
+
+
+def get_input():
+    while True:
+        try:
+            return float(input("Change owed: "))
+        except ValueError:
+            continue
+
+if __name__ == "__main__":
+    main()
