139a48907471927e9345e7c661832d9eeab6327a
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-11-26 15:25:33 +0000

    Sun, Nov 26, 2023, 6:25 PM +03:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -2,9 +2,24 @@ import re
 
 def main():
 
-    change = get_input()
+    change = int(get_input() * 100)
 
-    
+    coins = [25,10,5,1]
+    count = 0
+    idx = 0
+
+    while change > 0:
+        a = change - coins[idx]
+        if a > 0:
+            change = a
+            count += 1
+        elif a == 0:
+            break
+        else:
+            idx += 1
+
+    print(count)
+
 
 
 def get_input():
