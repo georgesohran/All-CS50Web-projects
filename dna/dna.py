e92eed41f397c46457d69916b5e04e2981f220c4
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-11-27 15:24:51 +0000

    Mon, Nov 27, 2023, 6:24 PM +03:00

--- dna/dna.py
+++ dna/dna.py
@@ -5,13 +5,13 @@ import sys
 def main():
 
     # TODO: Check for command-line usage
-    if len(sys.argv) != 4:
+    if len(sys.argv) != 3:
         sys.exit("Usage: python dna.py data.csv sequence.txt")
 
     # TODO: Read database file into a variable
     people = []
 
-    with open(sys.argv[2],"r") as database:
+    with open(sys.argv[1],"r") as database:
         reader = csv.DictReader(database)
         for i in reader:
             people.append(i)
