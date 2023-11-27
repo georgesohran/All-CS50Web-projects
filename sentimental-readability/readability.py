4db959da17fefc3b9a2b9f5d84f8856113b95bb3
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-11-27 14:11:15 +0000

    Mon, Nov 27, 2023, 5:11 PM +03:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,7 +1,33 @@
 def main():
+
+    text = str(input("Text: "))
+
+    sentances = count_sentances(text)
+    words = count_words(text)
+    letters = count_letters(text)
+
+
+
+def count_sentances(txt:str):
+    count = 0
+
+    for i in range(len(txt)-1):
+        if txt[i] in [".","?","!"] and txt[i+1] != ".":
+            count += 1
+    return count
+
+
+def count_words(txt:str):
+    count = 0
+
+    for i in range(len(txt)-1):
+        if txt[i] in [".","?","!",","," ","",".","?","!"] and txt[i+1] != ".":
+            count += 1
+    return count
+
+
+def count_letters(txt:str):
     ...
-
-
 
 if __name__ == "__main__":
     main()
