17724a8edaeacef47d6d1e3626f13b89ef57c112
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-12-03 14:30:26 +0000

    Sun, Dec 3, 2023, 5:30 PM +03:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -11,3 +11,6 @@ SELECT transcript,name FROM interviews WHERE month == 7 AND day == 28;
 -- withdraw money, drove away from bakery parking lot, flew away tomorrow
 
 SELECT activity,license_plate FROM bakery_security_logs WHERE month == 7 AND day == 28 AND hour == 10 ;
+
+SELECT atm_location FROM atm_transactions GROUP BY atm_location;
+-- Leggett Street 
