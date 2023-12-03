d9b94538300f5e89c5c82f419f847225042d47f4
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-12-03 14:04:03 +0000

    Sun, Dec 3, 2023, 5:04 PM +03:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,7 +1,9 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
--- July 28, Humphrey street
+-- 07.28, Humphrey street
 -- name, place tp leave
 
 -- sus people:
 
 SELECT description FROM crime_scene_reports WHERE month == 7 AND day == 28;
+--time: 10:15, three withnesses
+
