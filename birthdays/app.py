0985719718ff06298441a7b3bde49a99339aaf49
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-12-12 13:19:59 +0000

    Tue, Dec 12, 2023, 4:19 PM +03:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -38,8 +38,8 @@ def index():
     else:
 
         # TODO: Display the entries in the database on index.html
-        
+        people = db.execute("SELECT * FROM birthdays")
 
-        return render_template("index.html")
+        return render_template("index.html", people=people)
 
 
