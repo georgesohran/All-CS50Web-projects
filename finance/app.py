cc412154b211e5d87421c421e1be4f6820f67f14
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-12-17 13:19:32 +0000

    Sun, Dec 17, 2023, 4:19 PM +03:00

--- finance/app.py
+++ finance/app.py
@@ -231,7 +231,7 @@ def sell():
             return apology("invalid shares", 403)
 
         symbol_share = db.execute("SELECT shares FROM users_stocks WHERE user_id == ? AND WHERE symbol = ?",session["user_id"][0]["id"],request.form.get("symbol"))
-        if and request.form.get("shares") > symbol_share[0]["share"]:
+        if request.form.get("shares") > symbol_share[0]["share"]:
             return apology("invalid amount of shares", 403)
 
         l = lookup(request.form.get("symbol"))
