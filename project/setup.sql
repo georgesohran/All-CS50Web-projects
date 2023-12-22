fc10d2226d8a1338af26e7b519366d0c591977fb
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-12-22 11:25:59 +0000

    Fri, Dec 22, 2023, 2:25 PM +03:00

--- project/setup.sql
+++ project/setup.sql
@@ -1,1 +1,23 @@
-CREATE TABLE
+CREATE TABLE students
+(
+    name TEXT NOT NULL,
+    id INTEGER NOT NULL,
+    password_hash TEXT NOT NULL,
+    
+);
+
+CREATE TABLE teachers
+(
+
+);
+
+CREATE TABLE subjects
+(
+
+);
+
+CREATE TABLE students_grades
+(
+
+);
+
