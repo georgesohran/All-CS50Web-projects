edc9d12bf6163a28fdb01aa1b1c8b8b0ff6b0edb
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-12-22 11:41:00 +0000

    Fri, Dec 22, 2023, 2:41 PM +03:00

--- project/setup.sql
+++ project/setup.sql
@@ -1,6 +1,6 @@
 CREATE TABLE students
 (
-    id INTEGER NOT NULL AUTO_INCREMENT=100,
+    id INTEGER NOT NULL AUTO_INCREMENT=200,
     name TEXT NOT NULL,
     password_hash TEXT NOT NULL,
     PRIMARY KEY (id)
@@ -11,16 +11,25 @@ CREATE TABLE teachers
     id INTEGER NOT NULL AUTO_INCREMENT,
     name TEXT NOT NULL,
     password_hash TEXT NOT NULL,
-    PRIMARY KEY (id)
+    subject_id INTEGER NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (subject_id) REFERENCES subjects(id)
 );
 
 CREATE TABLE subjects
 (
-
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    name TEXT NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE students_grades
 (
-
+    student_id INTEGER NOT NULL,
+    subject_id INTEGER NOT NULL,
+    grade INTEGER NOT NULL,
+    time TEXT NOT NULL,
+    FOREIGN KEY (student_id) REFERENCES students(id),
+    FOREIGN KEY (subject_id) REFERENCES subjects(id)
 );
 
