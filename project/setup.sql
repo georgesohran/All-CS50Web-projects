96edd4b59adc8811c6f6a57505bf21b36b592189
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-12-22 11:56:22 +0000

    Fri, Dec 22, 2023, 2:56 PM +03:00

--- project/setup.sql
+++ project/setup.sql
@@ -2,8 +2,7 @@ CREATE TABLE students
 (
     id INTEGER PRIMARY KEY  AUTOINCREMENT(200) NOT NULL,
     name TEXT NOT NULL,
-    password_hash TEXT NOT NULL,
-
+    password_hash TEXT NOT NULL
 );
 
 CREATE TABLE teachers
@@ -18,7 +17,7 @@ CREATE TABLE teachers
 CREATE TABLE subjects
 (
     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    name TEXT NOT NULL,
+    name TEXT NOT NULL
 );
 
 CREATE TABLE students_grades
