32461ea8ce842e8dea60b931654c61d7c8029825
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-12-22 12:24:44 +0000

    Fri, Dec 22, 2023, 3:24 PM +03:00

--- project/app.py
+++ project/app.py
@@ -6,7 +6,7 @@ app = FastAPI()
 db = sqlite3.connect("database.db")
 cur = db.cursor()
 
-cur.execute("INSERT INTO subjects (name) VALUES (history);")
+cur.execute("INSERT INTO subjects VALUES (name,english),(name,math),(name,history);")
 db.commit()
 
 @app.get("/")
