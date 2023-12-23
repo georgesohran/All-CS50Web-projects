3f98a0ea47b0dc4010d5b56117006369839ba624
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-12-23 14:10:07 +0000

    Sat, Dec 23, 2023, 5:10 PM +03:00

--- project/app.py
+++ project/app.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 import sqlite3
 
 app = FastAPI()
