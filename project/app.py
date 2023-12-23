3b2b2e51c87f9f0bbb3f5f7edd619838754e1b94
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-12-23 14:26:02 +0000

    Sat, Dec 23, 2023, 5:26 PM +03:00

--- project/app.py
+++ project/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
@@ -12,7 +12,7 @@ cur = db.cursor()
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
-def index():
+def index(request: Request):
     return templates.TemplateResponse("index.html", {"word":"my word"})
 
 
