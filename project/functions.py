216d111905950b96431cd4769e8e88b95c3a192b
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-12-24 11:44:01 +0000

    Sun, Dec 24, 2023, 2:44 PM +03:00

--- project/functions.py
+++ project/functions.py
@@ -3,4 +3,14 @@ from fastapi.responses import RedirectResponse
 
 from pydantic import BaseModel
 
+from functools import wraps
 
+
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return RedirectResponse("/login")
+        return f(*args, **kwargs)
+    return decorated_function
