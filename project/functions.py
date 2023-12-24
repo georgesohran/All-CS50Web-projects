f53c1e65b2b5ef96f343c801d15aaae457178f33
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-12-24 11:22:42 +0000

    Sun, Dec 24, 2023, 2:22 PM +03:00

--- project/functions.py
+++ project/functions.py
@@ -5,3 +5,11 @@ from pydantic import BaseModel
 
 class SessionData(BaseModel):
     userid: str
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return RedirectResponse("/login")
+        return f(*args, **kwargs)
+    return decorated_function
