61ec2b96458975c9ac5d848cbd538fca990619bd
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-12-25 05:27:40 +0000

    Mon, Dec 25, 2023, 8:27 AM +03:00

--- project/app.py
+++ project/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, flash, redirect, render_template, request, session
+from functions import login_required
 
 
 app = Flask(__name__)
 
 
 @app.route("/")
+@login_required
 def index():
     return render_template("index.html")
