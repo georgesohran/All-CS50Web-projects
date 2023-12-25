40dfb263b5b152ad7eaa1bc17e89202414a45bc1
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-12-25 05:15:09 +0000

    Mon, Dec 25, 2023, 8:15 AM +03:00

--- project/app.py
+++ project/app.py
@@ -1,2 +1,9 @@
-from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session
 
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
