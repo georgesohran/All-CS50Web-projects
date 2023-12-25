036e01fd066236f1f8e14f24bee7f0e671545fb1
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-12-25 05:35:16 +0000

    Mon, Dec 25, 2023, 8:35 AM +03:00

--- project/app.py
+++ project/app.py
@@ -1,9 +1,20 @@
 from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
 from functions import login_required
 
 
 app = Flask(__name__)
 
+Session(app)
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
 
 @app.route("/")
 @login_required
