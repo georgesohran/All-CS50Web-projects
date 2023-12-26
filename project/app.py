fb49ee0e4ef309749e88b592517206ed2439233c
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-12-26 16:13:00 +0000

    Tue, Dec 26, 2023, 7:13 PM +03:00

--- project/app.py
+++ project/app.py
@@ -57,6 +57,7 @@ def register():
 
         if type == "teacher":
             subject_id = db.execute("SELECT id FROM subjects WHERE name == ?", (subject,)).fetchall()
+            print(subject_id)
             if (subject,) not in db.execute("SELECT name FROM subjects").fetchall():
                 return render_template("register.html",messege="invalid subject")
             cur.execute("INSERT INTO teachers (name,password_hash,subject_id) VALUES(?,?,?)",(name, generate_password_hash(password), subject_id))
