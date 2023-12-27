1c9be440d072c0823d840c146dc675fb99d24903
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-12-27 13:56:55 +0000

    Wed, Dec 27, 2023, 4:56 PM +03:00

--- project/app.py
+++ project/app.py
@@ -111,8 +111,9 @@ def login():
         if name not in all_names:
             return render_template("login.html", messege="the name is not registrated")
 
-        act_password = cur.execute(f"SELECT password_hash FROM {type}s WHERE name == ?", (name,)).fetchall()
-        if check_password_hash(act_password, password):
+        act_password = cur.execute(f"SELECT password_hash FROM {type}s WHERE name == ?", (name,)).fetchall()[0]
+        print(act_password[0], generate_password_hash(password))
+        if check_password_hash(act_password[0], password):
             return render_template("login.html", messege="invalid password")
 
         if type == "teacher":
