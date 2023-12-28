969257f5e425c7c1e6735d253abf9993a3218645
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-12-28 13:04:41 +0000

    Thu, Dec 28, 2023, 4:04 PM +03:00

--- project/app.py
+++ project/app.py
@@ -143,7 +143,7 @@ def main_page():
 
         bad_subjects = db.execute("""SELECT name FROM subjects WHERE id IN
                                   (SELECT subject_id FROM students_grades WHERE student_id == ? AND
-                                  SELECT AVG(grade) < 4) """, (session["user_id"][0][0],)).fetchall()
+                                  AVG(grade) < 4) """, (session["user_id"][0][0],)).fetchall()
 
         bad_subjects = [sub[0] for sub in bad_subjects]
 
