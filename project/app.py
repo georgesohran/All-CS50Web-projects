b0d84f37c6793ab01e252588cc7dd70e8ef75aee
georgesohran <116193314+georgesohran@users.noreply.github.com>
2023-12-28 13:22:41 +0000

    Thu, Dec 28, 2023, 4:22 PM +03:00

--- project/app.py
+++ project/app.py
@@ -146,7 +146,8 @@ def main_page():
 
         bad_sub_id = db.execute("SELECT subject_id FROM students_grades WHERE student_id == ?", (session["user_id"][0][0],)).fetchall()
 
-        bad_subjects = db.execute("SELECT name FROM subjects WHERE id IN (?)", (bad_sub_id[0][0],)).fetchall()
+        for sub in bad_sub_id:
+            bad_subjects = db.execute("SELECT name FROM subjects WHERE id IN (?)", (bad_sub_id[0][0],)).fetchall()
 
         bad_subjects = [sub[0] for sub in bad_subjects]
 
