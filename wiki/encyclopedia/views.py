d3251d1671c678ae4101f4346b52e67c954fd68b
georgesohran <116193314+georgesohran@users.noreply.github.com>
2024-01-13 11:57:28 +0000

    Sat, Jan 13, 2024, 2:57 PM +03:00

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -25,13 +25,10 @@ def index(request):
                 "entries": util.list_entries()
             })
 
-        
-
-
-
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
 
 def entry(request, name):
     if md_content := util.get_entry(name):
