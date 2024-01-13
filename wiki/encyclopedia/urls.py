8c34bd90010e0d5415e0ab4199d7154cb21a691c
georgesohran <116193314+georgesohran@users.noreply.github.com>
2024-01-13 07:18:58 +0000

    Sat, Jan 13, 2024, 10:18 AM +03:00

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("<str:name>", views.entry, name="entry")
+    path("search_results", views.search, name="search_results")
 ]
