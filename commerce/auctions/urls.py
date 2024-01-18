1b953b9f011e16efc698917e068e246a4e9346d2
georgesohran <116193314+georgesohran@users.noreply.github.com>
2024-01-18 14:50:19 +0000

    Thu, Jan 18, 2024, 5:50 PM +03:00

--- commerce/auctions/urls.py
+++ commerce/auctions/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
-    path("listings/<int:id>", views.)
+    path("listings/<int:auction_id>", views.listings, name="listings")
 ]
