db961e31d324d5ac396f2fc9edb4d6fdc741a8bc
georgesohran <116193314+georgesohran@users.noreply.github.com>
2024-01-20 14:52:53 +0000

    Sat, Jan 20, 2024, 5:52 PM +03:00

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -13,7 +13,7 @@ class Bid(models.Model):
 
 class Auction(models.Model):
     host_id = models.ForeignKey(User, models.CASCADE)
-    current_bid_id = models.ForeignKey(Bid, models.CASCADE)
+    # current_bid_id = models.ForeignKey(Bid, models.CASCADE)
     product = models.CharField(max_length=64)
     description = models.CharField(max_length=256)
     image = models.ImageField()
