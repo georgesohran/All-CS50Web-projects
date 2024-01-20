087f9049bc4d2b9c52e95fb47124631c2a854631
georgesohran <116193314+georgesohran@users.noreply.github.com>
2024-01-20 15:11:46 +0000

    Sat, Jan 20, 2024, 6:11 PM +03:00

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -19,6 +19,7 @@ class Auction(models.Model):
     # current_bid_id = models.ForeignKey(Bid, models.CASCADE)
     product = models.CharField(max_length=64)
     description = models.CharField(max_length=256)
+    category = models.CharField(max_length=64)
     image = models.ImageField(blank=True)
     time = models.CharField()
 
@@ -26,10 +27,16 @@ class Auction(models.Model):
         return f"host: {self.host.id}\nproduct: {self.product}\ntime: {self.time}"
 
 
-class Auction_Bids(models.Model):
+class Auction_Bid(models.Model):
     auction_id = models.ForeignKey(Auction, models.CASCADE)
     bid_id = models.ForeignKey(Bid, models.CASCADE)
 
     def __str__(self):
         return f"auction: {self.auction_id}\nbid: {self.bid_id}"
 
+class Comment(models.Model):
+    user_id = models.ForeignKey(User, models.CASCADE)
+    contents = models.CharField(max_length=1024)
+
+    def __str__(self):
+        return f"user: {self.user_id}\ncontents: {self.contents}"
