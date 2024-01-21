4a4661c27179e53cab8b4d2fac21aca1b08a5429
georgesohran <116193314+georgesohran@users.noreply.github.com>
2024-01-21 15:37:28 +0000

    Sun, Jan 21, 2024, 6:37 PM +03:00

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -5,11 +5,13 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 
-from .models import User
+from .models import *
 
 @login_required(login_url="/login")
 def index(request):
-    return render(request, "auctions/index.html")
+    return render(request, "auctions/index.html",{
+        "auctions":Auction.objects.all()
+    })
 
 
 def login_view(request):
