9dc88f775560533d257337a579ddb416bea37950
georgesohran <116193314+georgesohran@users.noreply.github.com>
2024-01-22 16:21:33 +0000

    Mon, Jan 22, 2024, 7:21 PM +03:00

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -6,9 +6,17 @@ class User(AbstractUser):
     pass
 
 
+class Bid(models.Model):
+    user_id = models.ForeignKey(User, models.CASCADE)
+    bid_price = models.IntegerField()
+
+    def __str__(self):
+        return f"user_id: {self.user_id},\nbid_price: {self.bid_price}"
+
+
 class Auction(models.Model):
     host_id = models.ForeignKey(User, models.CASCADE)
-    # current_bid_id = models.ForeignKey(Bid, models.CASCADE)
+    current_bid_id = models.ForeignKey(Bid, models.CASCADE)
     product = models.CharField(max_length=64)
     description = models.CharField(max_length=256)
     category = models.CharField(max_length=64, blank=True)
@@ -17,15 +25,6 @@ class Auction(models.Model):
 
     def __str__(self):
         return f"host: {self.host_id},\nproduct: {self.product},\ntime: {self.time}"
-
-
-class Bid(models.Model):
-    auction_id = models.ForeignKey(Auction, models.CASCADE)
-    user_id = models.ForeignKey(User, models.CASCADE)
-    bid_price = models.IntegerField()
-
-    def __str__(self):
-        return f"user_id: {self.user_id},\nbid_price: {self.bid_price}"
 
 
 class Comment(models.Model):
