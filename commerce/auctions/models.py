64dbb30d074b2339bcf00f1c6f09dc30afd0b774
georgesohran <116193314+georgesohran@users.noreply.github.com>
2024-01-23 14:51:54 +0000

    Tue, Jan 23, 2024, 5:51 PM +03:00

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -13,7 +13,6 @@ class Auction(models.Model):
     category = models.CharField(max_length=64, blank=True)
     image = models.ImageField(blank=True)
     time = models.CharField(max_length=64, blank=True)
-    bid_count = models
 
     def __str__(self):
         return f"host: {self.host_id},\nproduct: {self.product},\ntime: {self.time}"
@@ -25,7 +24,7 @@ class Bid(models.Model):
     bid_price = models.IntegerField()
 
     def __str__(self):
-        return f"user_id: {self.user_id},\nbid_price: {self.bid_price}"
+        return f"user: {self.user_id},\nbid_price: {self.bid_price}"
 
 
 class Comment(models.Model):
