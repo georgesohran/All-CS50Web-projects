ecdf508d1f4b58cd8f1a795dfb92368600845d7d
georgesohran <116193314+georgesohran@users.noreply.github.com>
2024-01-23 14:53:15 +0000

    Tue, Jan 23, 2024, 5:53 PM +03:00

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -13,6 +13,7 @@ class Auction(models.Model):
     category = models.CharField(max_length=64, blank=True)
     image = models.ImageField(blank=True)
     time = models.CharField(max_length=64, blank=True)
+    current_price = models.FloatField()
 
     def __str__(self):
         return f"host: {self.host_id},\nproduct: {self.product},\ntime: {self.time}"
