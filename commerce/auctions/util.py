0beb7accae930a22a67e2d894fb1abb31a49f221
georgesohran <116193314+georgesohran@users.noreply.github.com>
2024-01-23 15:33:58 +0000

    Tue, Jan 23, 2024, 6:33 PM +03:00

--- commerce/auctions/util.py
+++ commerce/auctions/util.py
@@ -5,9 +5,9 @@ import pytz
 utc=pytz.UTC
 
 def get_latest_time(a):
-    max_time = datetime.datetime(2000,1,1,tzinfo=tz)
+    max_time = datetime.datetime(2000,1,1,tzinfo=utc)
     for obj in a:
         time = obj.time.replace(tzinfo=utc)
-        if obj.time > max_time:
+        if time > max_time:
             max_time = obj.time
     return max
