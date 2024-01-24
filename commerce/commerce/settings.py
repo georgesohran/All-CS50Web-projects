b00843175476342f4bdf2e8e87f03eaf84247de0
georgesohran <116193314+georgesohran@users.noreply.github.com>
2024-01-24 16:00:24 +0000

    Wed, Jan 24, 2024, 7:00 PM +03:00

--- commerce/commerce/settings.py
+++ commerce/commerce/settings.py
@@ -121,5 +121,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+STATIC_FILES_DIRS = (
+    os.path.join(BASE_DIR,"static")
+)
