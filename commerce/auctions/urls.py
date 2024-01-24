46d41b82007094804425f495494f83d6870cefe7
georgesohran <116193314+georgesohran@users.noreply.github.com>
2024-01-24 12:09:19 +0000

    Wed, Jan 24, 2024, 3:09 PM +03:00

--- commerce/auctions/urls.py
+++ commerce/auctions/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+from . import settings
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
@@ -10,5 +12,5 @@ urlpatterns = [
     path("categories", views.categories, name="categories"),
     path("watchlist", views.watchlist, name="watchlist"),
     path("create_listing", views.create_listing, name="create_listing"),
-    path("listings/<int:auction_id>", views.listings, name="listings")
+    path("listings/<int:auction_id>", views.listings, name="listings"),
 ]
