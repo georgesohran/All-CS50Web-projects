5fc033187440278e53dc2d598904e36f0071461e
georgesohran <116193314+georgesohran@users.noreply.github.com>
2024-01-27 16:34:31 +0000

    Sat, Jan 27, 2024, 7:34 PM +03:00

--- commerce/auctions/urls.py
+++ commerce/auctions/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path("watchlist", views.watchlist, name="watchlist"),
     path("create_listing", views.create_listing, name="create_listing"),
     path("listings/<int:listing_id>", views.listings, name="listing"),
-    path("<str:category>", views.index, name="index_cat")
+
 ]
