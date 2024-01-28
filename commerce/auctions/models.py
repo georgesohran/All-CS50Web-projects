70999456f1c83f785c80430bb244b8d4175d1063
georgesohran <116193314+georgesohran@users.noreply.github.com>
2024-01-28 16:08:03 +0000

    Sun, Jan 28, 2024, 7:08 PM +03:00

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -8,6 +8,7 @@ class User(AbstractUser):
 
 class Auction(models.Model):
     host = models.ForeignKey(User, models.CASCADE, related_name="host")
+    winner = models.ForeignKey(User, models.CASCADE, related_name="winner")
     product = models.CharField(max_length=64)
     description = models.CharField(max_length=256)
     category = models.CharField(max_length=64, blank=True)
--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -130,8 +130,7 @@ def listings(request, listing_id):
         #if the user is trying to close an auction
         if "close" in request.POST and is_host:
             winner = bids.get(time=get_latest_time(bids)).user
-            auction.winner = winner
-            auction.save()
+
             return HttpResponseRedirect(reverse("index"))
 
 
