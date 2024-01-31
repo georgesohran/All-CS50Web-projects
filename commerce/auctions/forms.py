3863013324bce7df08f97e6f7e3ad3b92d686e1b
georgesohran <116193314+georgesohran@users.noreply.github.com>
2024-01-31 11:26:00 +0000

    Wed, Jan 31, 2024, 2:26 PM +03:00

--- commerce/auctions/forms.py
+++ commerce/auctions/forms.py
@@ -1,8 +1,13 @@
 from django.forms import ModelForm
-from .models import Bid
+from .models import Bid, Auction
 
 
 class BidForm(ModelForm):
     class Meta:
         model = Bid
         fields = ["bid_price"]
+
+class AuctionForm(ModelForm):
+    class Meta:
+        model = Auction
+        
