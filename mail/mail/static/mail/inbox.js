1b4ae5e740a5b184933715d381e5a2bdc5b06026
georgesohran <116193314+georgesohran@users.noreply.github.com>
2024-02-14 11:39:05 +0000

    Wed, Feb 14, 2024, 2:39 PM +03:00

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -23,10 +23,12 @@ function compose_email() {
 }
 
 function load_mailbox(mailbox) {
-  
+
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+
+  fetch(`email/${mailbox}`).then(response => response.json()).then
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
