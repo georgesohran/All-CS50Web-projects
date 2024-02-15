e16a9cd208b166074151852985c560fee707a705
georgesohran <116193314+georgesohran@users.noreply.github.com>
2024-02-15 03:28:24 +0000

    Thu, Feb 15, 2024, 6:28 AM +03:00

--- mail/mail/admin.py
+++ mail/mail/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.register(User)
-admin.register(Email)
+admin.site.register(User)
+admin.site.register(Email)
