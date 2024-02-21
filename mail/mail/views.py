c8d2296aadf5dfb0d99a8bd6efae7a639d068cfe
georgesohran <116193314+georgesohran@users.noreply.github.com>
2024-02-21 06:06:06 +0000

    Wed, Feb 21, 2024, 9:06 AM +03:00

--- mail/mail/views.py
+++ mail/mail/views.py
@@ -110,6 +110,7 @@ def email(request, email_id):
 
     # Return email contents
     if request.method == "GET":
+        print(email.serialize()["body"])
         return JsonResponse(email.serialize())
 
     # Update whether email is read or should be archived
