344da73787b287ff28d818d378faaddf8d87ee18
georgesohran <116193314+georgesohran@users.noreply.github.com>
2024-02-22 09:51:36 +0000

    Thu, Feb 22, 2024, 12:51 PM +03:00

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -27,11 +27,11 @@ function compose_email() {
     console.log('click')
     fetch('/emails', {
       method: 'POST',
-      body: `
+      body: JSON.stringify({
           recipients: 'baz@example.com',
           subject: 'Meeting time',
           body: 'How about we meet tomorrow at 3pm?'
-      `,
+      }),
       headers: {
         "Content-type": "application/json; charset=UTF-8"
       }
