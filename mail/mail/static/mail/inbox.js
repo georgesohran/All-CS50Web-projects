7d1c78e2a0e2ab3b5c18885d2289a384d7b53c06
georgesohran <116193314+georgesohran@users.noreply.github.com>
2024-02-22 10:32:24 +0000

    Thu, Feb 22, 2024, 1:32 PM +03:00

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -135,6 +135,18 @@ function load_mailbox(mailbox) {
               <div class="email-info-time">${email.timestamp}</div>
             </div>
         `;
+      } else if(mailbox == 'sent'){
+        newEmail.innerHTML = `
+            <div class="email-list-element">
+              <div class="email-info-cell"><button class="btn btn-sm btn-outline-primary" onclick="load_email(${email.id})"> See inside </button></div>
+              <div class="email-info-cell" style="padding-top:2px">
+                <b>${email.sender}</b>:&nbsp&nbsp
+                <span style="font-size:110%">${email.subject}</span>
+                ${email.archived ? '&nbsp&nbsp&nbsp<span style="font-size:110%">ARCHIVED</span>':''}
+              </div>
+              <div class="email-info-time">${email.timestamp}</div>
+            </div>
+        `;
       } else if(!email.archived && mailbox != 'archive') {
         newEmail.innerHTML = `
             <div class="email-list-element" ${email.read ? 'style="color:gray;border-color:gray"':''}>
