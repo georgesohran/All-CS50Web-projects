f26080ac5d804061a94550466fe47e251dce8e31
georgesohran <116193314+georgesohran@users.noreply.github.com>
2024-02-28 11:30:00 +0000

    Wed, Feb 28, 2024, 2:30 PM +03:00

--- network/network/models.py
+++ network/network/models.py
@@ -4,6 +4,9 @@ from django.db import models
 
 class User(AbstractUser):
     image = models.ImageField(blank=True,upload_to="images/")
-    
 
+
+
+class Posts(models.Model):
+    creator = models.ForeignKey(User, models.CASCADE, related_name="creator")
 
