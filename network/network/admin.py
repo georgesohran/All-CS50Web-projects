04d9de880bc29ef1b32b463ee1addd3f7d48ffc2
georgesohran <116193314+georgesohran@users.noreply.github.com>
2024-03-02 16:50:15 +0000

    Sat, Mar 2, 2024, 7:50 PM +03:00

--- network/network/admin.py
+++ network/network/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.register(User)
-admin.register(Posts)
-admin.register(Comments)
+admin.site.register(User)
+admin.site.register(Posts)
+admin.site.register(Comments)
