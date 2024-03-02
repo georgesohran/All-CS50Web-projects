5ba9be221f709c123ef6c542abd345499f6752e1
georgesohran <116193314+georgesohran@users.noreply.github.com>
2024-03-02 15:55:20 +0000

    Sat, Mar 2, 2024, 6:55 PM +03:00

--- network/network/views.py
+++ network/network/views.py
@@ -5,6 +5,7 @@ from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
 
 from .models import *
 
@@ -81,3 +82,8 @@ def profile(request, id):
     })
 
 # API routes are here:
+
+@csrf_exempt
+@login_required
+def api_make_post(request):
+    return
