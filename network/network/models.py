c487102393e9bd2281dae8cf36c37744ce155f67
georgesohran <116193314+georgesohran@users.noreply.github.com>
2024-03-02 16:31:22 +0000

    Sat, Mar 2, 2024, 7:31 PM +03:00

--- network/network/models.py
+++ network/network/models.py
@@ -6,6 +6,9 @@ class User(AbstractUser):
     image = models.ImageField(blank=True,upload_to="images/")
     followers = models.ManyToManyField("self", blank=True, null=True)
 
+    def __str__(self):
+        return f"{self.email}"
+
 
 class Posts(models.Model):
     creator = models.ForeignKey(User, models.CASCADE, related_name="creator_post")
@@ -15,12 +18,19 @@ class Posts(models.Model):
     likes = models.IntegerField()
     dislikes = models.IntegerField()
 
+    def __str__(self):
+        return f"{self.creator}, {self.title} : {self.timestamp}"
+
 
 class Comments(models.Model):
     creator = models.ForeignKey(User, models.CASCADE, related_name="creator_comment")
-    commented_post = models.ForeignKey(User, models.CASCADE, related_name="com_post")
+    commented_post = models.ForeignKey(User, models.CASCADE, related_name="commented_post")
     body = models.CharField(max_length=1024)
     likes = models.IntegerField()
     dislikes = models.IntegerField()
+    timestamp = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.creator} : {self.timestamp}"
 
 
