7e5de9e7dcff3e10f85a4b0d89d69c4a80b9669c
georgesohran <116193314+georgesohran@users.noreply.github.com>
2024-03-03 14:19:20 +0000

    Sun, Mar 3, 2024, 5:19 PM +03:00

--- network/network/views.py
+++ network/network/views.py
@@ -67,7 +67,7 @@ def register(request):
 
 
 def index(request):
-    posts = Post.objects.exclude(user=request.user)
+    posts = Post.objects.all().exclude(user=request.user)
     return render(request, "network/index.html", {
         "posts":posts
     })
