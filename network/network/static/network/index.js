3f4dd26512e76e3fb91a8a82c4978b675b47f7a7
georgesohran <116193314+georgesohran@users.noreply.github.com>
2024-03-04 16:35:56 +0000

    Mon, Mar 4, 2024, 7:35 PM +03:00

--- network/network/static/network/index.js
+++ network/network/static/network/index.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('nem-post-form').addEventListener('submit', () => {
-        fetch("/api_make_post").then(response => response.json()).then(() => )
+        fetch("/api_make_post").then(response => response.json()).then((result) => {
+            console.log(result)
+        })
     })
 })
