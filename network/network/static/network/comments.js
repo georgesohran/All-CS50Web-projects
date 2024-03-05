15b4a741ea8a64063b476150425b5f9e980a92c7
georgesohran <116193314+georgesohran@users.noreply.github.com>
2024-03-05 16:52:15 +0000

    Tue, Mar 5, 2024, 7:52 PM +03:00

--- network/network/static/network/comments.js
+++ network/network/static/network/comments.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('#post-container-n').forEach((container) => {
         const num = container.dataset.num
-        let comment_button = document.querySelector(`[data-num="${num}]`)
-        let cancel_button = document.querySelector(`[data-num="${num}]`)
-        let comment_text = document.querySelector(`[data-num="${num}]`)
+        let comment_button = document.querySelector(`#comment-button-${num}`)
+        let cancel_button = document.querySelector(`#cancel-button-${num}`)
+        let comment_text = document.querySelector(`#comment-text-${num}`)
 
         console.log(comment_button)
     })
