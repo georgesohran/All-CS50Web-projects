94c4d5770110c886001dadccd0fabbc9da28c701
georgesohran <116193314+georgesohran@users.noreply.github.com>
2024-03-05 16:14:34 +0000

    Tue, Mar 5, 2024, 7:14 PM +03:00

--- network/network/static/network/comments.js
+++ network/network/static/network/comments.js
@@ -1,24 +1,4 @@
-document.addEventListener('DOMContentLoaded' () => {
-    document.querySelectorAll('#post-container').forEach((container) => {
-        let comment_button = container.getElementById('comment-button')
-        comment_button.onclick = () => {
-            let c_button = container.getElementById('cancel-button')
-            let c_text = container.getElementById('comment-text')
-
-            c_button.style.display = 'block'
-            c_text.style.display = 'block'
-            comment_button.onclick = make_comment
-
-            c_button.onclick = () => {cancel_comment(c_button, c_text); }
-        }
-    })
+document.addEventListener('DOMContentLoaded', () => {
+    
 })
 
-function cancel_comment(btn, txt) {
-    btn.style.display = 'none'
-    txt.style.display = 'none'
-}
-
-function make_comment() {
-
-}
