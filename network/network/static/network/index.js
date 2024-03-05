f3334ef7aff125af1d661e33b1be3403c8e47d3f
georgesohran <116193314+georgesohran@users.noreply.github.com>
2024-03-05 15:03:36 +0000

    Tue, Mar 5, 2024, 6:03 PM +03:00

--- network/network/static/network/index.js
+++ network/network/static/network/index.js
@@ -1,12 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('new-post-form').addEventListener('submit', (event) => {
         event.preventDefault()
-        console.log('click')
         fetch('/api_make_post', {
                 method:"POST",
                 body:JSON.stringify({contents : document.getElementById('contents').value})
         }).then(response => response.json()).then((result) => {
-            console.log(result)
+            document.getElementById('message').value = result.message
         })
     })
 })
