883242b5234d9a10cb9e415d28b3aa0b91116b78
georgesohran <116193314+georgesohran@users.noreply.github.com>
2024-03-06 15:52:07 +0000

    Wed, Mar 6, 2024, 6:52 PM +03:00

--- network/network/static/network/comments.js
+++ network/network/static/network/comments.js
@@ -1,14 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('#post-container-n').forEach((container) => {
         const num = container.dataset.num
+        document.querySelector(`#cancel-button-${num}`).onclick = () => {cancel_comment(num)}
         document.querySelector(`#comment-button-${num}`).onclick = () => {show_comment_field(num)}
     })
 })
 
 function show_comment_field(num) {
-    document.querySelector(`#comment-button-${num}`)
+    document.querySelector(`#comment-text-${num}`).style.display = 'block'
+    document.querySelector(`#cancel-button-${num}`).style.display = 'block'
+
+    document.querySelector(`#comment-button-${num}`).onclick = () => {
+        //fetch something idk
+    }
 }
 
 function cancel_comment(num) {
+    document.querySelector(`#comment-text-${num}`).style.display = 'none'
+    document.querySelector(`#cancel-button-${num}`).style.display = 'none'
 
+    document.querySelector(`#comment-button-${num}`).onclick = () => {show_comment_field(num)}
 }
