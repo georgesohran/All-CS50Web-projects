167f6e71430fd6a81a6fdcf8b7b32c50838b4f1c
georgesohran <116193314+georgesohran@users.noreply.github.com>
2024-03-07 11:35:43 +0000

    Thu, Mar 7, 2024, 2:35 PM +03:00

--- network/network/static/network/like.js
+++ network/network/static/network/like.js
@@ -1,7 +1,11 @@
 //implement likes here
 
 function like() {
-    
+    document.querySelectorAll('#post-container-n').forEach((container) => {
+        const num = container.dataset.num
+        document.querySelector(`#cancel-button-${num}`).onclick = () => {cancel_comment(num)}
+        document.querySelector(`#comment-button-${num}`).onclick = () => {show_comment_field(num)}
+    })
 }
 
 function unlike() {
