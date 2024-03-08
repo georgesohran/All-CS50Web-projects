ac542031ce781824f9fc5e6d4cd32aa3478e2665
georgesohran <116193314+georgesohran@users.noreply.github.com>
2024-03-08 11:33:22 +0000

    Fri, Mar 8, 2024, 2:33 PM +03:00

--- network/network/static/network/profile.js
+++ network/network/static/network/profile.js
@@ -2,11 +2,6 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('#post-container-n').forEach((container) => {
         const num = container.dataset.num
 
-        document.querySelector(`#show-comments-${num}`).onclick = () => {show_comments(num)}
-
-        document.querySelector(`#cancel-button-${num}`).onclick = () => {cancel_comment(num)}
-        document.querySelector(`#comment-button-${num}`).onclick = () => {show_comment_field(num)}
-
-        document.querySelector(`#like-button-${num}`).onclick = () => {like(num)}
+        //get the edit button and make edit space
     })
 })
