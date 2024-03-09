2a5464c53ebc5c36d2ed29370914c0232ab7fa9f
georgesohran <116193314+georgesohran@users.noreply.github.com>
2024-03-09 08:53:20 +0000

    Sat, Mar 9, 2024, 11:53 AM +03:00

--- network/network/static/network/profile.js
+++ network/network/static/network/profile.js
@@ -2,13 +2,14 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('#post-container-n').forEach((container) => {
         const num = container.dataset.num
 
-        document.querySelector(`edit-button-${num}`).onclick = () => {show_edit_field}
+        document.querySelector(`#edit-button-${num}`).onclick = () => {show_edit_field}
         //get the edit button and make edit space
     })
 })
 
 function show_edit_field(num) {
-
+    document.querySelector(`#post-body-${num}`).style.display = 'none'
+    document.querySelector(`#edit-field-${num}`).style.display = 'block'
 }
 
 function cancel_edit_(num) {
