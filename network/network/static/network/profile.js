c7b7dd2f5eb8da8b3ec08c877b8fa89703c1064b
georgesohran <116193314+georgesohran@users.noreply.github.com>
2024-03-09 08:44:55 +0000

    Sat, Mar 9, 2024, 11:44 AM +03:00

--- network/network/static/network/profile.js
+++ network/network/static/network/profile.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('#post-container-n').forEach((container) => {
         const num = container.dataset.num
 
+        
         //get the edit button and make edit space
     })
 })
