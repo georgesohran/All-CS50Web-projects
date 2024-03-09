df47a970d6b8115d581c166023790d3700d7248f
georgesohran <116193314+georgesohran@users.noreply.github.com>
2024-03-09 09:17:35 +0000

    Sat, Mar 9, 2024, 12:17 PM +03:00

--- network/network/static/network/profile.js
+++ network/network/static/network/profile.js
@@ -3,16 +3,32 @@ document.addEventListener('DOMContentLoaded', () => {
         const num = container.dataset.num
 
         document.querySelector(`#edit-button-${num}`).onclick = () => {show_edit_field(num)}
-        //get the edit button and make edit space
     })
 })
 
 function show_edit_field(num) {
-    console.log(num)
     document.querySelector(`#post-body-${num}`).style.display = 'none'
     document.querySelector(`#edit-field-${num}`).style.display = 'block'
+
+    document.querySelector(`#edit-text-${num}`).innerHTML = document.querySelector(`#main-post-contents-${num}`).innerHTML
+
+    document.querySelector(`#save-edit-button-${num}`).onclick = () => {save_edit(num)}
+    document.querySelector(`#cancel-edit-button-${num}`).onclick = () => {cancel_edit(num)}
 }
 
-function cancel_edit_(num) {
+function cancel_edit(num) {
+    document.querySelector(`#post-body-${num}`).style.display = 'block'
+    document.querySelector(`#edit-field-${num}`).style.display = 'none'
+}
 
+function save_edit(num) {
+    new_contents = document.querySelector(`#edit-text-${num}`).value
+    fetch(`/api_make_post`, {
+        method:'PUT',
+        body:JSON.stringify({new_contents:new_contents, post_id:num})
+    }).then(response => response.json()).then((result) => {
+        document.querySelector(`#post-body-${num}`).style.display = 'block'
+        document.querySelector(`#edit-field-${num}`).style.display = 'none'
+        document.querySelector(`#main-post-contents-${num}`).innerHTML = new_contents
+    })
 }
